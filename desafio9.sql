4c3df7c4daf03d7e26490cdda44670e1e38ac707
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-07-22 19:26:02 -0300

    Correcting desafio9.sql changing sum to count

--- desafio9.sql
+++ desafio9.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(employees.firstname, ' ', employees.lastname) AS 'Nome completo', 
-SUM(orders.employeeid) AS 'Total de pedidos'
+COUNT(orders.employeeid) AS 'Total de pedidos'
 FROM w3schools.employees
 INNER JOIN w3schools.orders
-ON employees.EmployeeID = orders.employeeid
+ON orders.employeeid = employees.employeeid
 GROUP BY orders.employeeid
-ORDER BY SUM(orders.employeeid);
+ORDER BY COUNT(orders.employeeid);
