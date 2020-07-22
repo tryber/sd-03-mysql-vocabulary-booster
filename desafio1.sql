56862c269cf38bad59ee74ccb81fdfa0236d37a0
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-07-22 18:04:29 -0300

    ué, <> não é diferente?

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
 SELECT COUNTRY_NAME AS Pais,
         
     CASE
-    WHEN REGION_ID <> 1 THEN
-    'não incluído'
-    ELSE 'incluido'
+    WHEN REGION_ID = 1 THEN
+    'incluído'
+    ELSE 'Não incluido'
     END AS 'Status Inclusão'
 FROM hr.countries; 
