3ee529ad93baa3da7d14860295a8b9a3f1e94efa
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-07-22 18:29:33 -0300

    agora vai?

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,7 @@
-SELECT COUNTRY_NAME AS 'País', IF (REGION_ID = 1, 'incluído', 'não incluído') AS "Status Inclusão"
-FROM hr.countries; 
+SELECT country_name AS País,
+         IF (region_id = 
+    (SELECT region_id
+    FROM hr.regions
+    WHERE region_name = 'Europe'), 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries
+ORDER BY  País ASC;
