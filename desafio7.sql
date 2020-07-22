36a73481bca13ed29a14519615491a89cfcbf143
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-07-22 17:52:52 -0300

    Uploading desafio7.sql

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(employees.first_name, ' ', employees.last_name)) AS 'Nome completo',
+job_history.start_date AS 'Data de início do cargo',
+employees.salary AS 'Salário'
+FROM hr.employees
+INNER JOIN hr.job_history
+ON employees.employee_id = job_history.employee_id
+WHERE MONTH(job_history.start_date) IN (01, 02, 03)
+ORDER BY CONCAT(employees.first_name, ' ', employees.last_name), job_history.start_date;
