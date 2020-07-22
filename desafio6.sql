1357a307e6d9e4bab5d7f7015245c657f9d16fba
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-07-22 16:48:34 -0300

    Correcting desafio6.sql

--- desafio6.sql
+++ desafio6.sql
@@ -8,5 +8,5 @@ ON employees.employee_id = job_history.employee_id
 INNER JOIN hr.jobs
 ON job_history.job_id = jobs.job_id
 INNER JOIN hr.departments
-ON employees.department_id = departments.department_id
+ON job_history.department_id = departments.department_id
 ORDER BY CONCAT(employees.first_name, ' ', employees.last_name) DESC, jobs.job_title;
