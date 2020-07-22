d0b5fd01cb730e5fafd0a88b475681a374aaf177
William Igor <williamigorsilva@gmail.com>
2020-07-22 18:37:39 -0300

    verificando testes

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,2 @@
-SELECT COUNTRY_NAME AS País, 
-IF(REGION_ID = 1, 'incluido', 'não incluido') as 'Status Inclusão'  FROM hr.countries ORDER BY País;
+SELECT COUNTRY_NAME AS 'País', 
+IF(REGION_ID = 1, 'incluido', 'não incluido') as 'Status Inclusão'  FROM hr.countries ORDER BY COUNTRY_NAME;
