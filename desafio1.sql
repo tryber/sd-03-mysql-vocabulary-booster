6b59eb89919fa034cf9142f17407aa319d073769
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-07-22 17:12:58 -0300

    code climate

--- desafio1.sql
+++ desafio1.sql
@@ -4,5 +4,5 @@ SELECT
     country.REGION_ID = (SELECT reg.REGION_ID FROM hr.regions AS reg WHERE reg.REGION_NAME = "Europe"),
     "incluído",
     "não incluso"
-	) AS "Status Inclusão"
+  ) AS "Status Inclusão"
 FROM hr.countries AS country;
