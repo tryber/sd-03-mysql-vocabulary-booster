b2c24d3531e0000ad8f809a5c98094361b9a7a3d
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-22 16:57:29 -0300

    code climate

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
 SELECT
-	country.COUNTRY_NAME AS País,
-  IF (
-		country.REGION_ID = (SELECT reg.REGION_ID FROM hr.regions AS reg WHERE reg.REGION_NAME = "Europe"),
-        "incluído",
-        "não incluso"
-	) AS "Status Inclusão"
+country.COUNTRY_NAME AS País,
+IF (
+  country.REGION_ID = (SELECT reg.REGION_ID FROM hr.regions AS reg WHERE reg.REGION_NAME = "Europe"),
+  "incluído",
+  "não incluso"
+) AS "Status Inclusão"
 FROM hr.countries AS country;
