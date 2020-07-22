806e5f11b56ce7f7a10c1969e30841fcabc7c9d9
clayton Pereira <cpereiramt@gmail.com>
2020-07-22 16:15:46 -0400

    Salvando alterações codeclimate 02

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,4 @@
 SELECT countries.COUNTRY_NAME AS País, ( CASE WHEN region_id = '1' THEN 'incluído' WHEN region_id <> '1' THEN 'não incluído' END) AS  'Status Inclusão' FROM hr.countries;    
-    
+
+
+
