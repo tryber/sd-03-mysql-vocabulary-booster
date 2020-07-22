e21d49ef9f439a5f1b0cca951c734cca9e17f1b3
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-07-22 16:40:12 -0300

    Primeiro push

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
 j.JOB_TITLE AS 'Cargo',
-h.START_DATE AS 'Data de inicio do cargo',
+h.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.job_history AS h
 INNER JOIN hr.jobs AS j ON h.JOB_ID = j.JOB_ID
