6f64b65aee8ab3d1c4992c3423c07416263461fe
DevT <aguiarthiag@gmail.com>
2020-07-22 20:00:36 -0300

    Challenges 11, 16, 7

--- desafio16.sql
+++ desafio16.sql
@@ -1,6 +1,11 @@
 DELIMITER $$
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN emailin VARCHAR(100))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(emailin VARCHAR(100))
+RETURNS INT READS SQL DATA
 BEGIN
-    SELECT COUNT(*) FROM hr.job_history WHERE EMPLOYEE_ID = (SELECT EMPLOYEE_ID FROM hr.employees WHERE EMAIL = emailin);
+	DECLARE quant INT;
+    SELECT COUNT(*) FROM hr.job_history WHERE EMPLOYEE_ID = (SELECT EMPLOYEE_ID FROM hr.employees WHERE EMAIL = emailin) INTO quant;
+    RETURN quant;
 END $$
 DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('ASDASDAS');
