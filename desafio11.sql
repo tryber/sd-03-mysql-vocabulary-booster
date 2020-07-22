55b70609ce72898ce6cde7556d4c50c6b85d92d4
DevT <aguiarthiag@gmail.com>
2020-07-22 20:03:19 -0300

    CC

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT 
-	a.ContactName AS Nome,
-    a.Country AS `País`,
-    COUNT(*)
+a.ContactName AS Nome,
+a.Country AS `País`,
+COUNT(*)
 FROM w3schools.customers AS a, w3schools.customers AS b
 WHERE a.country = b.country AND a.ContactName <> b.ContactName
 GROUP BY a.ContactName, a.Country
