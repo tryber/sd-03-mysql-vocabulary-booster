60fbbcf70a915a234245966d5fa5d36d4f4c487d
Jafet Henrique <jafetguerra6@gmail.com>
2020-07-22 18:05:38 -0300

    cc

--- desafio6.sql
+++ desafio6.sql
@@ -3,10 +3,10 @@ SELECT * FROM hr.jobs;
 SELECT * FROM hr.employees;
 SELECT * FROM hr.departments;
 SELECT
-	 (SELECT CONCAT(first_name, ' ', last_name) FROM hr.employees WHERE employee_id = JH.employee_id) AS Nome_Completo,
-     (SELECT job_title FROM hr.jobs WHERE job_id = JH.job_id)AS Cargo,
-     start_date AS 'Data de início do cargo',
-     (SELECT department_name FROM hr.departments WHERE department_id = JH.department_id) AS Departamento
+	(SELECT CONCAT(first_name, ' ', last_name) FROM hr.employees WHERE employee_id = JH.employee_id) AS Nome_Completo,
+	(SELECT job_title FROM hr.jobs WHERE job_id = JH.job_id)AS Cargo,
+	start_date AS 'Data de início do cargo',
+	(SELECT department_name FROM hr.departments WHERE department_id = JH.department_id) AS Departamento
 FROM hr.job_history AS JH
 ORDER BY
 Nome_Completo DESC,
