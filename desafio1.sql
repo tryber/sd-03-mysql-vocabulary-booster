bec748bd5f219ced7db0e1bef85b50cb484b7283
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-07-22 14:44:35 -0300

    Uploading desafio1.sql

---
+++ desafio1.sql
@@ -0,0 +1,7 @@
+SELECT country_name AS País, (
+	SELECT IF(region_id = 1, 'incluído', 'não incluído')
+	FROM hr.regions
+    WHERE regions.region_id = countries.region_id
+) AS 'Status Inclusão'
+FROM hr.countries
+ORDER BY countries.country_name;
