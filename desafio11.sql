3ce275802005c1c1fcb8e59fa9fc01b345f35456
DevT <aguiarthiag@gmail.com>
2020-07-22 20:16:48 -0300

    11 & 7

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT 
 a.ContactName AS Nome,
 a.Country AS `País`,
-COUNT(*)
+COUNT(*) AS `Número de compatriotas`
 FROM w3schools.customers AS a, w3schools.customers AS b
 WHERE a.country = b.country AND a.ContactName <> b.ContactName
 GROUP BY a.ContactName, a.Country
