ff6f8fa80b4b37ba4f90d3e15a78efadefd10ab6
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-07-22 15:03:25 -0300

    vqv

---
+++ desafio1.sql
@@ -0,0 +1,9 @@
+SELECT
+	COUNTRY_NAME AS País,
+	f.* FROM hr.countries
+		INNER JOIN
+        (
+			SELECT IF(REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão'
+			FROM hr.regions
+		) AS f
+ORDER BY COUNTRY_NAME;
