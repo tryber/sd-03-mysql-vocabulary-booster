0bd28e68850559a4226a4678f24500997d8f6b7e
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-22 19:03:15 -0300

    CC 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,10 @@ SELECT
 CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS "Nome completo",
 (SELECT j.JOB_TITLE FROM hr.jobs AS j WHERE j.JOB_ID = e.JOB_ID LIMIT 1) AS Cargo,
 e.HIRE_DATE AS "Data de início do cargo",
-(SELECT d.DEPARTMENT_NAME FROM hr.departments AS d WHERE e.DEPARTMENT_ID = d.DEPARTMENT_ID LIMIT 1) AS "Departamento"
+(
+  SELECT d.DEPARTMENT_NAME FROM hr.departments AS d
+  WHERE e.DEPARTMENT_ID = d.DEPARTMENT_ID LIMIT 1
+) AS "Departamento"
 FROM hr.employees AS e
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, Cargo ASC;
 SELECT * FROM hr.employees
