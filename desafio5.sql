b97e014bd58063600ac60ede56ff6c3d0459ce1a
Jafet Henrique <jafetguerra6@gmail.com>
2020-07-22 18:11:53 -0300

    cc

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT 
-	job_title AS Cargo,
-	max_salary - min_salary  AS 'Variação Salarial',
-	ROUND(AVG(min_salary) / 12, 2) AS 'Média mínima mensal',
-	ROUND(AVG(max_salary) / 12, 2) AS 'Média máxima mensal'
+job_title AS Cargo,
+max_salary - min_salary  AS 'Variação Salarial',
+ROUND(AVG(min_salary) / 12, 2) AS 'Média mínima mensal',
+ROUND(AVG(max_salary) / 12, 2) AS 'Média máxima mensal'
 FROM hr.jobs
 GROUP BY job_id
 ORDER BY 
