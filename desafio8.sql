4b059e9060c7b191e2cddfe5cb2587c63a1fb7b1
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-22 20:42:45 -0300

    8.sql

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+SELECT
+(SELECT ContactName FROM w3schools.customers AS c WHERE c.CustomerID = o.CustomerID) AS `Nome de Contato`,
+ship.ShipperName AS `Empresa que fez o envio`,
+o.OrderDate AS "Data do Pedido"
+FROM w3schools.orders AS o
+INNER JOIN w3schools.shippers AS ship
+ON ship.ShipperID = o.ShipperID
+WHERE ship.ShipperName IN("Speedy Express", "United Package")
+ORDER BY `Nome de Contato`, ship.ShipperName, o.OrderDate;
