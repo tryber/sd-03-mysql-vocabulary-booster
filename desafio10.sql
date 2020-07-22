c3a40e21b1f07a6d7421f670684867c2db1761e7
tales <talestales@gmail.com>
2020-07-22 15:24:20 -0300

    pncdocc

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,9 @@
 SELECT
-	ProductID,
-    min(quantity) AS Mínimo,
-    Max(quantity) AS Máximo,
-    ROUND(AVG(Quantity),2) AS Média
-        FROM w3schools.OrderDetails
+ProductID,
+min(quantity) AS Mínimo,
+Max(quantity) AS Máximo,
+ROUND(AVG(Quantity),2) AS Média
+FROM w3schools.OrderDetails
 Group BY productID
 HAving AVG(Quantity) > 20
 ORDER BY AVG(Quantity);
--- desafio13.sql
+++ desafio13.sql
@@ -1,5 +1,5 @@
 SELECT ProductName AS Produto, Price AS Preço FROM w3schools.Products
 WHERE ProductID IN (
-	SELECT ProductID FROM orderdetails
-    WHERE quantity > 80)
-    ORDER BY 1;
+SELECT ProductID FROM orderdetails
+WHERE quantity > 80)
+ORDER BY 1;
