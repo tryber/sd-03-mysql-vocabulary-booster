8ca6c7e5af183d3a9f313614a7d6b366115369ad
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-07-22 15:56:03 -0300

    Uploading desafio5.sql and correcting desafio4.sql

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,9 @@
 SELECT jobs.job_title AS 'Cargo', ROUND(AVG(employees.salary), 2) AS 'Média salarial',
 CASE
-WHEN employees.salary BETWEEN 2000 AND 5800 THEN 'Júnior'
-WHEN employees.salary BETWEEN 5801 AND 7500 THEN 'Pleno'
-WHEN employees.salary BETWEEN 7501 AND 10500 THEN 'Sênior'
-WHEN employees.salary > 10500 THEN 'CEO'
+WHEN ROUND(AVG(employees.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN ROUND(AVG(employees.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN ROUND(AVG(employees.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN ROUND(AVG(employees.salary), 2) > 10500 THEN 'CEO'
 END AS 'Senioridade'
 FROM hr.jobs
 INNER JOIN hr.employees
