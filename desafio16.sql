16b73fc9aed2bd7a03f8eb48c37f662b2d7b605f
DevT <aguiarthiag@gmail.com>
2020-07-22 18:19:24 -0300

    Endlines

--- desafio16.sql
+++ desafio16.sql
@@ -4,5 +4,3 @@ BEGIN
     SELECT COUNT(*) FROM hr.job_history WHERE EMPLOYEE_ID = (SELECT EMPLOYEE_ID FROM hr.employees WHERE EMAIL = emailin);
 END $$
 DELIMITER ;
-
-CALL buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
