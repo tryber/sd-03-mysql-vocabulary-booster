6c7ecd398a695b5568c4d114f9d452533d593484
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-22 18:39:54 -0300

    desafio7

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT UCASE(CONCAT(E.FIRST_NAME,' ', E.LAST_NAME)) AS 'Nome completo',
-E.HIRE_DATE AS 'Data de início',
+H.START_DATE AS 'Data de início',
 E.SALARY AS 'Salário'
-FROM hr.employees AS E, hr.jobs AS J
-WHERE (E.JOB_ID = J.JOB_ID)
-ORDER BY CONCAT(E.FIRST_NAME,' ', E.LAST_NAME) ASC, E.HIRE_DATE ASC;
+FROM hr.employees AS E, hr.jobs AS J, hr.job_history AS H
+WHERE (E.JOB_ID = J.JOB_ID) AND (E.EMPLOYEE_ID = H.EMPLOYEE_ID)
+ORDER BY CONCAT(E.FIRST_NAME,' ', E.LAST_NAME) ASC, H.START_DATE ASC;
