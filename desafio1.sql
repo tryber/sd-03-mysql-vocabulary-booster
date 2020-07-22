1265fed6344a53c374775f1ca4b7788a8a313e68
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-22 18:13:15 -0300

    SOME CORRECTIONS UNTIOL 3

--- desafio1.sql
+++ desafio1.sql
@@ -3,6 +3,6 @@ country.COUNTRY_NAME AS País,
 IF (
   country.REGION_ID = (SELECT reg.REGION_ID FROM hr.regions AS reg WHERE reg.REGION_NAME = "Europe"),
   "incluído",
-  "não incluso"
+  "não incluído"
 ) AS "Status Inclusão"
 FROM hr.countries AS country;
