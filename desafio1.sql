4f333b8818ab3b579c4d1b1cef37cab4b62a36c0
William Igor <williamigorsilva@gmail.com>
2020-07-22 18:45:39 -0300

    faltou acento

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,2 @@
 SELECT COUNTRY_NAME AS 'País', 
-IF(REGION_ID = 1, 'incluido', 'não incluido') as 'Status Inclusão'  FROM hr.countries ORDER BY COUNTRY_NAME;
+IF(REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão'  FROM hr.countries ORDER BY COUNTRY_NAME;
