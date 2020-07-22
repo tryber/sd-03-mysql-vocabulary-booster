34f9b43b7a0e29f613ff4df957a010e87999ff30
DevT <aguiarthiag@gmail.com>
2020-07-22 20:30:44 -0300

    Vacilão

--- desafio7.sql
+++ desafio7.sql
@@ -6,4 +6,4 @@ FROM hr.employees AS e
 JOIN hr.job_history AS jh
 ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 WHERE MONTH(jh.start_date) IN (1, 2, 3)
-ORDER BY `Nome completo`, `Data de início do cargo`;
+ORDER BY `Nome completo`, `Data de início`;
