abaf1b21edd6c5f16a2615e9815a03e4bf184c52
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-22 18:30:11 -0300

    atualizando desafio6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(E.FIRST_NAME,' ', E.LAST_NAME) AS 'Nome completo',
 J.JOB_TITLE AS Cargo,
-E.HIRE_DATE AS 'Data de início do cargo',
+H.START_DATE AS 'Data de início do cargo',
 D.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees AS E, hr.jobs AS J, hr.departments AS D
-WHERE (E.JOB_ID = J.JOB_ID) AND (E.DEPARTMENT_ID = D.DEPARTMENT_ID)
+FROM hr.employees AS E, hr.jobs AS J, hr.departments AS D, hr.job_history AS H
+WHERE (E.JOB_ID = J.JOB_ID) AND (E.DEPARTMENT_ID = D.DEPARTMENT_ID) AND (E.EMPLOYEE_ID = H.EMPLOYEE_ID)
 ORDER BY CONCAT(E.FIRST_NAME,' ', E.LAST_NAME) DESC, Cargo ASC;
