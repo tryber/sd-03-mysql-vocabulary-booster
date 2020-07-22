4f7a5afe0790aa12924d8cbc826a9373a5b3a652
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-07-22 19:48:36 -0300

    challenges until 9

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT customers.ContactName AS `Nome de contato`,
+  shippers.ShipperName AS `Empresa que fez o envio`,
+  orders.OrderDate AS `Data do Pedido`
+FROM w3schools.orders AS orders
+  INNER JOIN w3schools.shippers AS shippers ON orders.ShipperID = shippers.ShipperID
+  INNER JOIN w3schools.customers AS customers ON orders.CustomerID = customers.CustomerID
+WHERE shippers.ShipperName IN('Speedy Express', 'United Package')
+ORDER BY customers.ContactName,
+  shippers.ShipperName,
+  orders.OrderDate;
