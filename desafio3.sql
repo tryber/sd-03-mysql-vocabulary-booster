207ce4e7263fcc35d290094ab31350c7474daf93
Henrique Rezende <henriquerezendec@gmail.com>
2020-07-22 19:05:07 -0300

    Desafio 4

--- desafio3.sql
+++ desafio3.sql
@@ -2,4 +2,5 @@ SELECT
   JOB_TITLE AS Cargo,
   MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
   FROM hr.jobs
-  ORDER BY MAX_SALARY - MIN_SALARY ASC;
+  ORDER BY 2 ASC,
+  JOB_TITLE;
