26feeee9617e59996b6ab8e5b132a4d635c0ef06
Hfreitas <hfreitas86@gmail.com>
2020-07-22 22:41:14 -0300

    tentativa de passar no teste

--- desafio8.sql
+++ desafio8.sql
@@ -5,6 +5,6 @@ FROM w3schools.orders AS orders
   INNER JOIN w3schools.shippers AS shippers ON orders.ShipperID = shippers.ShipperID
   INNER JOIN w3schools.customers AS customers ON orders.CustomerID = customers.CustomerID
 WHERE shippers.ShipperName IN('Speedy Express', 'United Package')
-ORDER BY `Nome de contato` ASC,
-  `Empresa que fez o envio` ASC,
-  `Data do Pedido` ASC;
+ORDER BY `Nome de contato`,
+  `Empresa que fez o envio`,
+  `Data do Pedido`;
