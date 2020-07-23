962ead4bad04c985d44ce165406fe531f2bdf3f5
nato-x <alvares.renato@hotmail.com>
2020-07-23 19:40:45 -0300

    d4

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT job_title AS 'Cargo', ROUND(AVG(employee.salary), 2)AS 'Média salarial',
+SELECT employee. AS 'Cargo', ROUND(AVG(employee.salary), 2)AS 'Média salarial',
     CASE 
         WHEN ROUND(AVG(employee.salary), 2) BETWEEN 2000 AND 5800  
             THEN 'Júnior'
@@ -7,7 +7,7 @@ SELECT job_title AS 'Cargo', ROUND(AVG(employee.salary), 2)AS 'Média salarial',
         WHEN ROUND(AVG(employee.salary), 2)     BETWEEN 7500 AND 10500
             THEN 'Sênior'
         ELSE 'CEO'
-    END
+    END AS 'Senioridade'
 FROM hr.jobs job
     INNER JOIN hr.employees AS employee
     ON job.job_id = employee.job_id
