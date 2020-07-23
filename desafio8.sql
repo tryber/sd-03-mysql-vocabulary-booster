0301e9e37acea04e0099d971138aab105fa5fe60
Hfreitas <hfreitas86@gmail.com>
2020-07-22 23:33:57 -0300

    quero passar nesse teste

--- desafio8.sql
+++ desafio8.sql
@@ -1,3 +1,16 @@
+-- SELECT customers.ContactName AS `Nome de contato`,
+--   shippers.ShipperName AS `Empresa que fez o envio`,
+--   orders.OrderData AS `Data do Pedido`
+-- FROM w3schools.orders AS orders
+--   INNER JOIN w3schools.shippers AS shippers ON orders.ShipperID = shippers.ShipperID
+--   INNER JOIN w3schools.customers AS customers ON orders.CustomerID = customers.CustomerID
+-- WHERE (
+--     shippers.ShipperName = 'Speedy Express'
+--     OR shippers.ShipperName = 'United Package'
+--   )
+-- ORDER BY `Nome de contato`,
+--   `Empresa que fez o envio`,
+--   `Data do Pedido`;
 SELECT customers.ContactName AS 'Nome de contato',
   shippers.ShipperName AS 'Empresa que fez o envio',
   orders.OrderData AS 'Data do pedido'
