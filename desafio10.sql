86ec812358e12fa9e1e692b6d347bb5063aab39f
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-22 22:03:40 -0300

    10.sql

--- desafio10.sql
+++ desafio10.sql
@@ -2,7 +2,8 @@ SELECT
 (SELECT prod.ProductName FROM w3schools.products AS prod WHERE prod.ProductID = ordDet.ProductID) AS `Produto`,
 MAX(ordDet.Quantity) AS `Mínima`,
 MIN(ordDet.Quantity) AS `Máxima`,
-ROUND(AVG(ordDet.Quantity)) AS `Média`
+ROUND(AVG(ordDet.Quantity), 2) AS `Média`
 FROM  w3schools.order_details AS ordDet
 GROUP BY `Produto`
-HAVING `Média` > 20.00;
+HAVING `Média` > 20.00
+ORDER BY `Média`, `Produto`;
