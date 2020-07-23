bee410a1961a0ab93f8899c6a5f9f2bf11bfd470
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-07-23 20:47:29 -0300

    Create desafio9.sql

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+select (
+		select concat(FirstName, ' ', LastName)
+		from w3schools.employees
+    where EmployeeID = o.EmployeeID
+	) as 'Nome completo',
+count(*) as 'Total de pedidos'
+from w3schools.orders as o
+group by `Nome completo`
+order by `Total de pedidos`;
