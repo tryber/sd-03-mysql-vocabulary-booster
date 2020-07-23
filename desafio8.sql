c4de7efd927d2e3dd295e2bc34db00b414d35e9e
Hfreitas <hfreitas86@gmail.com>
2020-07-22 23:36:38 -0300

    não sei mais o que fazer

--- desafio8.sql
+++ desafio8.sql
@@ -11,13 +11,14 @@
 -- ORDER BY `Nome de contato`,
 --   `Empresa que fez o envio`,
 --   `Data do Pedido`;
-SELECT customers.ContactName AS 'Nome de contato',
-  shippers.ShipperName AS 'Empresa que fez o envio',
-  orders.OrderData AS 'Data do pedido'
+ 
+SELECT customers.contactname AS 'Nome de contato',
+shippers.shippername AS 'Empresa que fez o envio',
+orders.orderdate AS 'Data do pedido'
 FROM w3schools.orders
-  INNER JOIN w3schools.shippers ON orders.ShipperID = shippers.ShipperID
-  INNER JOIN w3schools.customers ON orders.CustomerID = customers.CustomerID
-WHERE shippers.ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY customers.ContactName,
-  shippers.ShipperName,
-  orders.OrderData;
+INNER JOIN w3schools.shippers
+ON orders.shipperid = shippers.shipperid
+INNER JOIN w3schools.customers
+ON orders.CustomerID = customers.CustomerID
+WHERE shippers.shippername IN ('Speedy Express', 'United Package')
+ORDER BY customers.contactname, shippers.shippername, orders.orderdate;
