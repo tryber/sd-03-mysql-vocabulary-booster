ead24dea67ef9049e668925b136c3684a19b5330
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-07-23 20:21:49 -0300

    bla

--- desafio1.sql
+++ desafio1.sql
@@ -2,5 +2,5 @@ SELECT COUNTRY_NAME AS País,
 IF(REGION_ID = (SELECT REGION_ID
 FROM hr.regions
 WHERE REGION_ID = 1),
-	'incluído', 'não incluído') as 'Status Inclusão' FROM hr.countries
+'incluído', 'não incluído') as 'Status Inclusão' FROM hr.countries
 ORDER BY COUNTRY_NAME;
