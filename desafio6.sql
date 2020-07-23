5b692b971294afa37e08d741bdb96455a82359eb
Henrique Rezende <henriquerezendec@gmail.com>
2020-07-23 19:40:11 -0300

    Fixes

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,25 @@
 SELECT
-  (SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) FROM hr.employees WHERE employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID) AS `Nome completo`,
-  (SELECT JOB_TITLE FROM hr.jobs WHERE jobs.JOB_ID = job_history.JOB_ID) AS Cargo,
-  START_DATE AS "Data de início do cargo",
-  (SELECT DEPARTMENT_NAME FROM hr.departments WHERE departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID)AS Departamento
-FROM hr.job_history
+    (SELECT
+            CONCAT(FIRST_NAME, ' ', LAST_NAME)
+        FROM
+            hr.employees
+        WHERE
+            employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID) AS `Nome completo`,
+    (SELECT
+            JOB_TITLE
+        FROM
+            hr.jobs
+        WHERE
+            jobs.JOB_ID = job_history.JOB_ID) AS Cargo,
+    START_DATE AS 'Data de início do cargo',
+    (SELECT
+            DEPARTMENT_NAME
+        FROM
+            hr.departments
+        WHERE
+            departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID) AS Departamento
+FROM
+    hr.job_history
+WHERE
+    YEAR(START_DATE) <> 0
 ORDER BY `Nome completo` ASC;
