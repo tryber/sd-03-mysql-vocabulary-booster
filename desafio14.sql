ffa1819ab6c1a17eaaddc776efaae43adf333747
Hfreitas <hfreitas86@gmail.com>
2020-07-23 12:56:54 -0300

    desafio 14 ok

---
+++ desafio14.sql
@@ -0,0 +1,15 @@
+(
+  SELECT Country AS 'País'
+  FROM w3schools.customers
+  ORDER BY Country
+  LIMIT 5
+)
+UNION
+(
+  SELECT Country
+  FROM w3schools.suppliers
+  ORDER BY Country
+  LIMIT 5
+)
+ORDER BY 'País'
+LIMIT 5;
