30752a1a51cf768864f1faaf9cec8189d4c23267
Hfreitas <hfreitas86@gmail.com>
2020-07-23 19:45:01 -0300

    tentando

--- desafio15.sql
+++ desafio15.sql
@@ -1,5 +1,6 @@
 USE hr; 
-/* DELIMITER $$
+
+DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN JobTitle VARCHAR(30))
 BEGIN
@@ -11,18 +12,5 @@ BEGIN
 END $$ 
 
 DELIMITER;
-
-CALL buscar_media_por_cargo('Programmer'); */
-
-DELIMITER $$
-CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30))
-BEGIN
-  SELECT ROUND(AVG(emp.SALARY), 2) AS 'Média salarial'
-  FROM hr.employees AS emp
-  INNER JOIN hr.jobs AS job ON job.JOB_ID = emp.JOB_ID
-  WHERE job.JOB_TITLE = cargo
-  GROUP BY job.JOB_TITLE;
-END $$
-DELIMITER ;
 
 CALL buscar_media_por_cargo('Programmer');
