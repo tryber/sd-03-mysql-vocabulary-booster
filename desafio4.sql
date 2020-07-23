eb6d8a1bd8a80f65d1d150dc3c008de17a5b8ee0
fabemiliano <fab.emiliano@gmail.com>
2020-07-23 14:05:04 -0300

    fixed cc

--- desafio4.sql
+++ desafio4.sql
@@ -4,7 +4,7 @@ when media between 5801 and 7500 then 'Pleno'
 when media between 7501 and 10500 then 'Sênior'
 else 'CEO'
 end as Senioridade
- from (select job_title as Cargo, round(average,2) as media
+from (select job_title as Cargo, round(average,2) as media
 from jobs
 inner join (select job_id, avg(salary) as average from employees group by job_id) as media
 on jobs.job_id = media.job_id) as primeira
