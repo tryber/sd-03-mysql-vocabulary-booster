a0b5757bd7ad1f3d329c1e57af9fd56f7dda13fb
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-23 14:05:13 -0300

    15.sql

---
+++ tests/challengesResults/desafio15.sql
@@ -0,0 +1,7 @@
+DELIMITER $$ CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30)) BEGIN
+SELECT ROUND(AVG(emp.SALARY))
+FROM hr.employees AS emp
+  INNER JOIN hr.jobs AS job ON job.JOB_ID = emp.JOB_ID
+WHERE job.JOB_TITLE = cargo
+GROUP BY job.JOB_TITLE;
+END $$ DELIMITER;
