4cd2def05929597815886829b20df9304bc6d07a
Henrique Rezende <henriquerezendec@gmail.com>
2020-07-23 15:50:48 -0300

    CC

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,11 @@
 SELECT
-	  UCASE(CONCAT(E.FIRST_NAME, '', E.LAST_NAME)) AS `Nome completo`,
+    UCASE(CONCAT(E.FIRST_NAME, '', E.LAST_NAME)) AS `Nome completo`,
     JH.START_DATE AS `Data início`,
     E.SALARY AS Salário
-FROM hr.job_history AS JH INNER JOIN hr.employees AS E
-WHERE JH.EMPLOYEE_ID = E.EMPLOYEE_ID AND MONTH(JH.START_DATE) IN (01, 02, 03);
+FROM
+    hr.job_history AS JH
+        INNER JOIN
+    hr.employees AS E
+WHERE
+    JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+        AND MONTH(JH.START_DATE) IN (01, 02, 03);
