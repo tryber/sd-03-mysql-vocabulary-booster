4b326a9c6ce70b74657ed0c081746f3ab2b74bac
Hfreitas <hfreitas86@gmail.com>
2020-07-23 10:19:12 -0300

    make my code readable again!

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 SELECT jobs.JOB_TITLE AS 'Cargo',
-  ROUND(AVG(employees.SALARY), 2) AS 'Média salarial',
+  ROUND(AVG(employees.SALARY), 2) AS `Média salarial`,
   CASE
     WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
     WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
@@ -9,5 +9,5 @@ SELECT jobs.JOB_TITLE AS 'Cargo',
 FROM hr.jobs AS jobs
   JOIN hr.employees AS employees ON jobs.JOB_ID = employees.JOB_ID
 GROUP BY jobs.JOB_TITLE
-ORDER BY ROUND(AVG(employees.SALARY), 2),
+ORDER BY `Média salarial`,
   jobs.JOB_TITLE;
--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,10 @@
-SELECT customers.ContactName AS 'Nome de contato',
-  shippers.ShipperName AS 'Empresa que fez o envio',
-  orders.OrderDate AS 'Data do pedido'
+SELECT customers.ContactName AS `Nome de contato`,
+  shippers.ShipperName AS `Empresa que fez o envio`,
+  orders.OrderDate AS `Data do pedido`
 FROM w3schools.orders
   INNER JOIN w3schools.shippers ON orders.ShipperID = shippers.ShipperID
   INNER JOIN w3schools.customers ON orders.CustomerID = customers.CustomerID
 WHERE shippers.ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY customers.ContactName,
-  shippers.ShipperName,
-  orders.OrderDate;
+ORDER BY `Nome de contato`,
+  `Empresa que fez o envio`,
+  `Data do pedido`;
