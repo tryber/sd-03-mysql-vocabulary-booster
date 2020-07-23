0bd4356a5b40dc7aa6a9465c25d525d4dc8119a9
clayton Pereira <cpereiramt@gmail.com>
2020-07-22 20:33:10 -0400

    Salvando alterações no desafio 04

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
 SELECT countries.COUNTRY_NAME AS País,
 ( CASE WHEN region_id = '1' THEN 'incluído' WHEN region_id <> '1' THEN 'não incluído' END) AS  'Status Inclusão' 
-FROM hr.countries ORDER BY País;
+FROM hr.countries ORDER BY País;    
