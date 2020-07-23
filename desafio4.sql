2951f1b90ba775558bd8f8a29aacfbb631103348
clayton Pereira <cpereiramt@gmail.com>
2020-07-22 20:38:10 -0400

    Salvando alterações no desafio 04

--- desafio4.sql
+++ desafio4.sql
@@ -2,12 +2,12 @@ SELECT JOB_TITLE AS Cargo, (SELECT ROUND(AVG(salary),2)
 FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) AS 'Média salarial',
 (CASE WHEN (SELECT ROUND(AVG(salary),2) 
 FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) BETWEEN 2000 AND 5800 THEN 'Júnior'
- WHEN (SELECT ROUND(AVG(salary),2) 
+  WHEN (SELECT ROUND(AVG(salary),2) 
 FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID)BETWEEN 5801 AND 7500 THEN 'Pleno'
- WHEN (SELECT ROUND(AVG(salary),2) 
+  WHEN (SELECT ROUND(AVG(salary),2) 
 FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID)BETWEEN 7501 AND 10500 THEN 'Sênior'
- WHEN (SELECT ROUND(AVG(salary),2) 
+  WHEN (SELECT ROUND(AVG(salary),2) 
 FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) > 10500 THEN 'CEO' 
- END) AS Senioridade
+  END) AS Senioridade
 FROM hr.jobs order by (SELECT ROUND(AVG(salary),2) 
 FROM hr.employees WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID) ASC;
