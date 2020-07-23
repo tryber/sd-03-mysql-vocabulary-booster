335eec5b5bbb99bd69d74eee47c9c076dc839861
nato-x <alvares.renato@hotmail.com>
2020-07-23 19:42:27 -0300

    D5

--- desafio5.sql
+++ desafio5.sql
@@ -2,5 +2,4 @@ SELECT job_title AS 'Cargo',
     max_salary - min_salary AS 'Variação Salarial',
     ROUND(min_salary/ 12, 2) AS 'Média mínima mensal', ROUND(max_salary/12, 2 ) AS 'Média mínima mensal'
 FROM hr.jobs
-GROUP BY job_title
 ORDER BY `Variação Salarial`, job_title;
