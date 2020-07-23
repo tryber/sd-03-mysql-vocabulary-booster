4fef8f92737a35c99b468b20ecb1ae51bea58bf7
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-23 14:09:20 -0300

    running

--- tests/challengesResults/desafio15.sql
+++ tests/challengesResults/desafio15.sql
@@ -1,7 +1,10 @@
-DELIMITER $$ CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30)) BEGIN
-SELECT ROUND(AVG(emp.SALARY), 2)
-FROM hr.employees AS emp
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30))
+BEGIN
+  SELECT ROUND(AVG(emp.SALARY), 2)
+  FROM hr.employees AS emp
   INNER JOIN hr.jobs AS job ON job.JOB_ID = emp.JOB_ID
-WHERE job.JOB_TITLE = cargo
-GROUP BY job.JOB_TITLE;
-END $$ DELIMITER;
+  WHERE job.JOB_TITLE = cargo
+  GROUP BY job.JOB_TITLE;
+END $$
+DELIMITER ;
