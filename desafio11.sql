75b1cf3ec2a998239ac937a36f90070ff7abd374
nato-x <alvares.renato@hotmail.com>
2020-07-23 16:41:28 -0300

    CC fix

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 SELECT customerName AS Nome,
     country AS País,
-    (SELECT COUNT(country) FROM w3school.customers
-     WHERE country = customers.country) AS 'Número de compatriotas'
-FROM w3school.customers AS customers;
+    COUNT(country) AS 'Número de compatriotas'
+FROM w3schools.customers AS customers
+GROUP BY customerName;
--- desafio6.sql
+++ desafio6.sql
@@ -1,14 +1,21 @@
 SELECT CONCAT(first_name, ' ', last_name) AS 'Nome completo',
-    (SELECT job_title
-    FROM hr.jobs AS job
-    WHERE emp.job_id = job_id) AS Cargo,
+    (
+        SELECT job_title
+        FROM hr.jobs AS job
+        WHERE emp.job_id = job_id
+    ) AS Cargo,
     hire_date AS 'Data de início do cargo',
-    (SELECT DEPARTMENT_NAME
-    FROM hr.departments
-    WHERE emp.department_id LIKE department_id) AS 'Departamento'
+    (
+        SELECT DEPARTMENT_NAME
+        FROM hr.departments
+        WHERE emp.department_id LIKE department_id
+    ) AS 'Departamento'
 FROM hr.employees AS emp
-GROUP BY first_name, last_name
-ORDER BY CONCAT(first_name, ' ', last_name) DESC, (SELECT job_title
-FROM hr.jobs AS job
-WHERE emp.job_id = job_id);
-
+GROUP BY first_name,
+    last_name
+ORDER BY CONCAT(first_name, ' ', last_name) DESC,
+    (
+        SELECT job_title
+        FROM hr.jobs AS job
+        WHERE emp.job_id = job_id
+    );
