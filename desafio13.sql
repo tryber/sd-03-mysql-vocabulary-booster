fa94895c979fa532f7a33fea02d9d90c4d804ae0
Hfreitas <hfreitas86@gmail.com>
2020-07-23 12:44:25 -0300

    desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,10 @@
+SELECT products.ProductName AS 'Produto',
+  products.Price As 'Preço'
+FROM w3schools.products AS products
+WHERE (
+    SELECT order_details.Quantity
+    FROM w3schools.order_details as order_details
+    WHERE order_details.ProductID = products.ProductID
+      AND order_details.Quantity > 80
+  )
+ORDER BY products.ProductName;
