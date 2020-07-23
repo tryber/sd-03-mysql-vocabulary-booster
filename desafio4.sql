58a2976a63c7cc8b91643768196481137de4f342
nato-x <alvares.renato@hotmail.com>
2020-07-23 19:47:16 -0300

    D4 D5?

--- desafio4.sql
+++ desafio4.sql
@@ -7,7 +7,7 @@ SELECT employee. AS 'Cargo', ROUND(AVG(employee.salary), 2)AS 'Média salarial',
         WHEN ROUND(AVG(employee.salary), 2)     BETWEEN 7500 AND 10500
             THEN 'Sênior'
         ELSE 'CEO'
-    END AS 'Senioridade'
+    END AS Senioridade
 FROM hr.jobs job
     INNER JOIN hr.employees AS employee
     ON job.job_id = employee.job_id
