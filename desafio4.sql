40c1aa860c7b60afac041513db35c56b9d9e79db
nato-x <alvares.renato@hotmail.com>
2020-07-23 20:24:05 -0300

    CC fix

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 SELECT job_title AS 'Cargo', ROUND(AVG(employee.salary), 2)AS 'Média salarial',
-CASE 
+    CASE 
     WHEN ROUND(AVG(employee.salary), 2) BETWEEN 2000 AND 5800  
         THEN 'Júnior'
     WHEN ROUND(AVG(employee.salary), 2) BETWEEN 5800 AND 7500
         THEN 'Pleno'
-     WHEN ROUND(AVG(employee.salary), 2) BETWEEN 7500 AND 10500
+    WHEN ROUND(AVG(employee.salary), 2) BETWEEN 7500 AND 10500
         THEN 'Sênior'
         ELSE 'CEO'
     END AS 'Senioridade'
