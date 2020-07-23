2fcdef3003191f24f4b9706a00274d349b2f99eb
Hfreitas <hfreitas86@gmail.com>
2020-07-23 20:30:31 -0300

    Por hoje chega.

--- desafio18.sql
+++ desafio18.sql
@@ -1,17 +1,15 @@
 USE hr;
-
 DELIMITER $$
 
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(month INT year INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE hired_total INT;
-SELECT COUNT(HIRE_DATE) FROM hr.employees
-WHERE MONTH(HIRE_DATE) = month AND YEAR(HIRE_DATE) = year
-INTO hired_total
-RETURN hired_total;
+    SELECT COUNT(HIRE_DATE) FROM hr.employees
+    WHERE MONTH(HIRE_DATE) = month AND YEAR(HIRE_DATE) = year INTO hired_total
+    RETURN hired_total;
 END $$
 
 DELIMITER ;
 
-SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6,1987);
+SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987);
