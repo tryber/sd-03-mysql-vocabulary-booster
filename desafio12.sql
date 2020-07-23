3210e8650048338bfa065956457fbc2ab318cf99
Rodrigo Brezolin <brezolinr@gmail.com>
2020-07-23 14:30:42 -0300

    GO CC

---
+++ desafio12.sql
@@ -0,0 +1,12 @@
+SELECT CONCAT(employees1.FIRST_NAME, ' ', employees1.LAST_NAME) AS `Nome completo funcionário 1`,
+employees1.SALARY AS `Salário funcionário 1`,
+employees1.PHONE_NUMBER AS `Telefone funcionário 1`,
+CONCAT(employees2.FIRST_NAME, ' ', employees2.LAST_NAME) AS `Nome completo funcionário 2`,
+employees2.SALARY AS `Salário funcionário 2`,
+employees2.PHONE_NUMBER AS `Telefone funcionário 2`
+FROM hr.employees AS employees1,
+hr.employees AS employees2
+WHERE employees1.JOB_ID = employees2.JOB_ID
+AND employees1.FIRST_NAME <> employees2.FIRST_NAME
+ORDER BY `Nome completo funcionário 1`,
+`Nome completo funcionário 2`;
