48c7b3e6cc071d318ff747a2bd27a899b7602b26
Hfreitas <hfreitas86@gmail.com>
2020-07-22 23:18:10 -0300

    consolidando alterações

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,6 @@
 -- SELECT customers.ContactName AS `Nome de contato`,
 --   shippers.ShipperName AS `Empresa que fez o envio`,
---   orders.OrderDate AS `Data do Pedido`
+--   orders.OrderData AS `Data do Pedido`
 -- FROM w3schools.orders AS orders
 --   INNER JOIN w3schools.shippers AS shippers ON orders.ShipperID = shippers.ShipperID
 --   INNER JOIN w3schools.customers AS customers ON orders.CustomerID = customers.CustomerID
@@ -11,14 +11,13 @@
 -- ORDER BY `Nome de contato`,
 --   `Empresa que fez o envio`,
 --   `Data do Pedido`;
-
-SELECT customers.contactname AS 'Nome de contato',
-shippers.shippername AS 'Empresa que fez o envio',
-orders.orderdate AS 'Data do pedido'
+SELECT customers.ContactName AS 'Nome de contato',
+  shippers.ShipperName AS 'Empresa que fez o envio',
+  orders.OrderData AS 'Data do pedido'
 FROM w3schools.orders
-INNER JOIN w3schools.shippers
-ON orders.shipperid = shippers.shipperid
-INNER JOIN w3schools.customers
-ON orders.CustomerID = customers.CustomerID
-WHERE shippers.shippername IN ('Speedy Express', 'United Package')
-ORDER BY customers.contactname, shippers.shippername, orders.orderdate;
+  INNER JOIN w3schools.shippers ON orders.ShipperID = shippers.ShipperID
+  INNER JOIN w3schools.customers ON orders.CustomerID = customers.CustomerID
+WHERE shippers.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY customers.ContactName,
+  shippers.ShipperName,
+  orders.OrderData;
