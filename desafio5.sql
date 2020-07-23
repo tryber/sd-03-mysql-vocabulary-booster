ba92750a32c8a5545f262c566aa97d1a1c53ef8c
clayton Pereira <cpereiramt@gmail.com>
2020-07-22 21:44:56 -0400

    Salvando alterações no desafio 05

--- desafio5.sql
+++ desafio5.sql
@@ -3,4 +3,4 @@ SELECT JOB_TITLE AS Cargo, (MAX_SALARY - MIN_SALARY ) AS 'Variação Salarial',
 (SELECT ROUND(AVG(MAX_SALARY / 12),2) FROM hr.jobs WHERE hr.employees.JOB_ID = jobs.JOB_ID) AS 'Média máxima mensal'
 FROM hr.jobs, hr.employees 
   WHERE jobs.JOB_ID = employees.JOB_ID group by Cargo
-ORDER BY (MAX_SALARY - MIN_SALARY ) ASC, Cargo, MIN_SALARY, MAX_SALARY;
+ORDER BY (MAX_SALARY - MIN_SALARY ) ASC, Cargo, hr.jobs.MAX_SALARY, hr.jobs.MIN_SALARY;
