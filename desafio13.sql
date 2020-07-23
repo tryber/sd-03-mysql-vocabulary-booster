92543a89fefee94e27e7566e740d3e3daf85cb18
Hfreitas <hfreitas86@gmail.com>
2020-07-23 13:47:59 -0300

    Tentando passar no teste

--- desafio13.sql
+++ desafio13.sql
@@ -1,10 +1,7 @@
 SELECT products.ProductName AS `Produto`,
   products.Price As 'Preço'
-FROM w3schools.products AS products
-WHERE (
-    SELECT order_details.Quantity
-    FROM w3schools.order_details as order_details
-    WHERE order_details.ProductID = products.ProductID
-      AND order_details.Quantity > 80
-  )
-ORDER BY `Produto` ASC;
+FROM w3schools.products AS products,
+  w3schools.order_details AS order_details
+WHERE products.ProductID = order_details.ProductID
+  AND order_details.Quantity > 80
+ORDER BY `Produto`;
