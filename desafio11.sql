36c12ca36287d186d9605c365706fcc54e621929
clayton Pereira <cpereiramt@gmail.com>
2020-07-23 20:51:07 -0400

    Salvando alterações no desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,6 @@
-  SELECT customersa.ContactName AS Nome, customersa.Country AS País, COUNT(customersa.CustomerName) AS 'Número de compatriotas' FROM w3schools.customers AS customersa,
+  SELECT customersa.ContactName AS Nome, COUNT(customersa.CustomerName) AS 'Número de compatriotas', customersa.Country AS País FROM w3schools.customers AS customersa,
   w3schools.customers AS customersb
   WHERE customersa.Country = customersb.Country AND customersa.CustomerName <> customersb.CustomerName
   GROUP BY customersa.Country,customersa.ContactName
   ORDER BY 1;
+ 
