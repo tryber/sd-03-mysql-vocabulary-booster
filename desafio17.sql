9ad71a2da7c53296d1ac3daea0ee15a5c5c0279a
Jafet Henrique <jafetguerra6@gmail.com>
2020-07-24 16:13:57 -0300

    requisito 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,7 +1,7 @@
 SELECT 
 CONCAT(E.first_name, ' ', E.last_name) AS 'Nome completo',
 CONCAT(DAY(JH.start_date), '/', SUBSTRING(JH.start_date, 6, 2), '/', YEAR(JH.start_date)) AS 'Data de início',
-CONCAT(DAY(JH.end_date), '/', SUBSTRING(JH.start_date, 6, 2), '/', YEAR(JH.end_date)) AS 'Data de rescisão',
+CONCAT(DAY(JH.end_date), '/', SUBSTRING(JH.end_date, 6, 2), '/', YEAR(JH.end_date)) AS 'Data de rescisão',
 ROUND(DATEDIFF(JH.end_date, JH.start_date) / 365, 2) AS 'Anos trabalhados'
 FROM hr.job_history AS JH
 INNER JOIN hr.employees AS E
