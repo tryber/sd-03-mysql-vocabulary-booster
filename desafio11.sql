1a9b76dd75f17e55e63764613748c12680c3d8a8
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-07-24 13:32:29 -0300

    Update desafio11.sql

--- desafio11.sql
+++ desafio11.sql
@@ -2,4 +2,5 @@ select a.ContactName as 'Nome',
 a.Country as 'País',
 (select count(*) from customers where a.Country = Country and a.ContactName <> ContactName) as 'Número de compatriotas'
 from w3schools.customers as a
+having `Número de compatriotas` >= 1
 order by a.ContactName;
