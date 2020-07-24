edf33e3180973979623c6f11ddc9a0700a0261e4
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-07-24 13:30:43 -0300

    Update desafio11.sql

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 select a.ContactName as 'Nome',
 a.Country as 'País',
-(select count(*) from customers where a.Country = Country and a.CustomerID <> CustomerID) as 'Número de compatriotas'
+(select count(*) from customers where a.Country = Country and a.ContactName <> ContactName) as 'Número de compatriotas'
 from w3schools.customers as a
 order by a.ContactName;
