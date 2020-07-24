365948a1b6f4a8df12b57abb87e802e0cd63fbdb
nato-x <alvares.renato@hotmail.com>
2020-07-24 20:56:43 -0300

    6 cc fix

--- desafio6.sql
+++ desafio6.sql
@@ -1,20 +1,24 @@
-SELECT (
-        SELECT CONCAT(first_name, ' ', last_name)
-    FROM hr.employees employee
-    WHERE employee.employee_id = history.employee_id
-    ) AS 'Nome completo',
-    (
-        SELECT job_title
-    FROM hr.jobs AS job
-    WHERE history.job_id = job.job_id
-    ) AS Cargo,
-    (
-        SELECT DEPARTMENT_NAME
-    FROM hr.departments dep
-    WHERE history.department_id LIKE dep.department_id
-    ) AS 'Departamento'
-FROM hr.job_history history
-WHERE history.employee_id <> 0
-ORDER BY `Nome completo` DESC,
-    Cargo;
-    
+SELECT 
+    (SELECT 
+            CONCAT(first_name, ' ', last_name)
+        FROM
+            hr.employees employee
+        WHERE
+            employee.employee_id = history.employee_id) AS 'Nome completo',
+    (SELECT 
+            job_title
+        FROM
+            hr.jobs AS job
+        WHERE
+            history.job_id = job.job_id) AS Cargo,
+    (SELECT 
+            DEPARTMENT_NAME
+        FROM
+            hr.departments dep
+        WHERE
+            history.department_id LIKE dep.department_id) AS 'Departamento'
+FROM
+    hr.job_history history
+WHERE
+    history.employee_id <> 0
+ORDER BY `Nome completo` DESC , Cargo;
