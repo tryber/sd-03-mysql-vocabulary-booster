63831e80702dcda59cf059fb29b9de0643f6a961
nato-x <alvares.renato@hotmail.com>
2020-07-24 20:54:54 -0300

    6,9 no pair

--- desafio6.sql
+++ desafio6.sql
@@ -1,36 +1,20 @@
-/* SELECT SELECT CONCAT(first_name, ' ', last_name) AS 'Nome completo',
-    (
-        SELECT job_title
-        FROM hr.jobs AS job
-        WHERE emp.job_id = job_id
-    ) AS Cargo,
-    hire_date AS 'Data de início do cargo',
-    (
-        SELECT DEPARTMENT_NAME
-        FROM hr.departments
-        WHERE emp.department_id LIKE department_id
-    ) AS 'Departamento'
-FROM hr.employees AS emp
-ORDER BY `Nome completo` DESC, Cargo; 
- */
 SELECT (
-     SELECT
-        CONCAT(first_name, ' ', last_name)
+        SELECT CONCAT(first_name, ' ', last_name)
     FROM hr.employees employee
     WHERE employee.employee_id = history.employee_id
-  )
-    AS 'Nome completo',
+    ) AS 'Nome completo',
     (
         SELECT job_title
     FROM hr.jobs AS job
     WHERE history.job_id = job.job_id
-    )AS Cargo,
+    ) AS Cargo,
     (
         SELECT DEPARTMENT_NAME
     FROM hr.departments dep
     WHERE history.department_id LIKE dep.department_id
     ) AS 'Departamento'
-
 FROM hr.job_history history
-WHERE  history.employee_id <> 0
-ORDER BY `Nome completo` DESC, Cargo;
+WHERE history.employee_id <> 0
+ORDER BY `Nome completo` DESC,
+    Cargo;
+    
--- desafio9.sql
+++ desafio9.sql
@@ -1,11 +1,15 @@
-SELECT 
-    CONCAT(firstName, ' ', lastName) AS 'Nome completo',
-    (SELECT 
+SELECT *
+FROM (
+SELECT
+        CONCAT(firstName, ' ', lastName) AS 'Nome completo',
+        (SELECT
             COUNT(employeeID)
         FROM
-            w3schools.orders
+            w3schools.orders orders
         WHERE
-            EmployeeID = employee.EmployeeID) AS 'Total de pedidos'
-FROM
-    w3schools.employees AS employee
+            EmployeeID = employee.EmployeeID ) AS 'Total de pedidos'
+    FROM
+        w3schools.employees AS employee
+) as A
+WHERE `Total de pedidos` <> 0
 ORDER BY `Total de pedidos`;
