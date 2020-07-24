d46c067233a54ef6e84a4d3931e39bcc79990a1b
clayton Pereira <cpereiramt@gmail.com>
2020-07-23 21:36:22 -0400

    Salvando alterações no desafio 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,9 +1,10 @@
- SELECT ProductName AS Produto, 
- products.Price AS Preço
- FROM w3schools.products
- INNER JOIN w3schools.order_details
- ON order_details.ProductID = products.ProductID
- WHERE w3schools.order_details.Quantity > 80.00
- GROUP BY products.ProductName
- ORDER BY 1;
- 
+  SELECT ProductName AS Produto, 
+  products.Price AS Preço
+  FROM w3schools.products
+  INNER JOIN w3schools.order_details
+  ON order_details.ProductID = products.ProductID
+  WHERE w3schools.order_details.Quantity > 80.00
+  GROUP BY products.ProductName
+  ORDER BY 1;
+
+  
