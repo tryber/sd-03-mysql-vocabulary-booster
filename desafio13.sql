72b2f69080f39a23cd20049f490ffecc5d9874d1
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-24 18:52:20 -0300

    refatorando desafio13

--- desafio13.sql
+++ desafio13.sql
@@ -3,7 +3,7 @@ P.ProductName AS 'Produto', P.Price AS 'Preço'
 FROM
 w3schools.order_details AS D, w3schools.products AS P
 WHERE
-D.Quantity > 80 AND
-D.ProductID = P.ProductID
+(D.ProductID = P.ProductID) AND
+(D.Quantity > 80)
 ORDER BY
 P.ProductName ASC;
