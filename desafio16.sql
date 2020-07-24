a02c9d1296509a9410d36cee229421f50efa3abd
Jafet Henrique <jafetguerra6@gmail.com>
2020-07-24 15:27:42 -0300

    cc

--- desafio16.sql
+++ desafio16.sql
@@ -4,11 +4,11 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(enteredEmail VARCHAR(30))
 RETURNS INT READS SQL DATA
 BEGIN
-    DECLARE quantity INT;
-    SELECT COUNT(*) FROM hr.job_history
-	WHERE employee_id = (SELECT employee_id FROM hr.employees WHERE email = enteredEmail)
-    INTO quantity;
-    RETURN quantity;
+DECLARE quantity INT;
+SELECT COUNT(*) FROM hr.job_history
+WHERE employee_id = (SELECT employee_id FROM hr.employees WHERE email = enteredEmail)
+INTO quantity;
+RETURN quantity;
 END $$
 
 DELIMITER ;
