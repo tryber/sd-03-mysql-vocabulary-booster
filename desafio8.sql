d6f1131b283ff1ed998e358de27b08eb41efc216
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-23 23:35:08 -0300

    letra maiuscula em 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
 SELECT
-(SELECT ContactName FROM w3schools.customers AS c WHERE c.CustomerID = o.CustomerID) AS `Nome de Contato`,
+(SELECT ContactName FROM w3schools.customers AS c WHERE c.CustomerID = o.CustomerID) AS `Nome de contato`,
 ship.ShipperName AS "Empresa que fez o envio",
-DATE_FORMAT(o.OrderDate, '%Y-%m-%d') AS "Data do Pedido"
+DATE_FORMAT(o.OrderDate, '%Y-%m-%d') AS "Data do pedido"
 FROM w3schools.orders AS o
 INNER JOIN w3schools.shippers AS ship
 ON ship.ShipperID = o.ShipperID
