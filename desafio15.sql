202323580cec71dcb81d47b83865842f4bbfc634
nato-x <alvares.renato@hotmail.com>
2020-07-24 19:12:00 -0300

    15 COM ROUND

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(25)) 
 BEGIN
-    SELECT AVG(salary) FROM hr.employees
+    SELECT ROUND(AVG(salary),2) FROM hr.employees
     WHERE job_id = (
         SELECT job_id FROM hr.jobs WHERE job_title LIKE cargo);
 END $$
