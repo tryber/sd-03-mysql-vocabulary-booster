4333dad1dd62a7fc49b04192c7d3ac29777ac51c
nato-x <alvares.renato@hotmail.com>
2020-07-24 13:36:59 -0300

    D10

--- desafio10.sql
+++ desafio10.sql
@@ -1,15 +1,13 @@
 SELECT ProductName AS 'Produto',
-
     (SELECT MAX(quantity)
     FROM w3schools.order_details
     WHERE ProductID = product.ProductID ) AS 'Máxima',
-    (SELECT ROUND(AVG(quantity), 2)
+    (SELECT ROUND(AVG(quantity), 2) AS media
     FROM w3schools.order_details
-    WHERE ProductID = product.ProductID ) AS Média,
+    WHERE ProductID = product.ProductID) AS 'Média',
     (SELECT MIN(quantity)
     FROM w3schools.order_details
     WHERE ProductID = product.ProductID ) AS 'Mínima'
 FROM w3schools.products AS product
-ORDER BY (SELECT ROUND(AVG(quantity), 2)
-FROM w3schools.order_details
-WHERE ProductID = product.ProductID ), ProductName;
+HAVING `Média` > 20
+ORDER BY `Média`, ProductName;
