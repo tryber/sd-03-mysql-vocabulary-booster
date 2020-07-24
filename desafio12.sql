d077d21e2cfb78f0d4647601bf9ab20fc079a55d
nato-x <alvares.renato@hotmail.com>
2020-07-24 16:54:41 -0300

    D12

---
+++ desafio12.sql
@@ -0,0 +1,13 @@
+
+SELECT
+    CONCAT(A.first_name, ' ', A.last_name) AS 'Nome completo funcionário 1',
+    A.salary AS 'Salário funcionário 1',
+    A.phone_number AS 'Telefone funcionário 1',
+
+    CONCAT(B.first_name, ' ', B.last_name) AS 'Nome completo funcionário 2',
+    B.salary AS 'Salário funcionário 2',
+    B.phone_number AS 'Telefone funcionário 2'
+FROM hr.employees A, hr.employees B
+WHERE CONCAT(A.first_name, ' ', A.last_name) <> CONCAT(B.first_name, ' ', B.last_name) AND A.job_id = B.job_id
+
+;
