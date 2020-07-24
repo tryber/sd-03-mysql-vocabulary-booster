10645e0ed2656e3074992fcf65ceb2d8beafd2cd
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-24 11:15:13 -0300

    cleaning 5

--- desafio5.sql
+++ desafio5.sql
@@ -5,7 +5,3 @@ ROUND(j.MIN_SALARY / 12, 2) AS "Média mínima mensal",
 ROUND(j.MAX_SALARY / 12, 2) AS "Média máxima mensal"
 FROM hr.jobs AS j
 ORDER BY ROUND(j.MAX_SALARY - j.MIN_SALARY, 2) ASC;
-
-SELECT * FROM jobs;
-SELECT * FROM employees;
-SELECT * FROM hr.job_history;
