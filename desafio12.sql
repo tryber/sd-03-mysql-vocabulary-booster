c7021ea739467f16b5e1426cb774ac95fd6bb041
potew <potew0@gmail.com>
2020-07-23 22:23:33 -0300

    Update desafio12.sql

--- desafio12.sql
+++ desafio12.sql
@@ -22,5 +22,5 @@ CONCAT(E2.FIRST_NAME, ' ', E2.LAST_NAME) AS `Nome completo funcionário 2`,
 E2.SALARY AS `Salário funcionário 2`,
 E2.PHONE_NUMBER AS `Telefone funcionário 2`
 FROM employees AS E1, employees AS E2
-WHERE E1.JOB_ID <> E2.JOB_ID AND (E1.FIRST_NAME, E1.LAST_NAME) <> (E2.FIRST_NAME, E2.LAST_NAME)
+WHERE E1.JOB_ID = E2.JOB_ID AND (E1.FIRST_NAME, E1.LAST_NAME) <> (E2.FIRST_NAME, E2.LAST_NAME)
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
