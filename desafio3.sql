e0cecd8594efaef52a0db61d9052e342147e429a
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-23 22:45:27 -0300

    3.sql order

--- desafio3.sql
+++ desafio3.sql
@@ -2,4 +2,6 @@ SELECT
 JOB_TITLE AS Cargo,
 MAX_SALARY - MIN_SALARY AS "Diferença entre salários máximo e mínimo"
 FROM hr.jobs
-ORDER BY MAX_SALARY - MIN_SALARY ASC, Cargo DESC;
+ORDER BY MAX_SALARY - MIN_SALARY ASC, Cargo ASC;
+SELECT * FROM hr.jobs;
+SELECT * FROM hr.employees;
