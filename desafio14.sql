f92713c12d38f7f76cfd30a9b9c8cd9e3a64d935
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-23 23:01:20 -0300

    14 aliás correcting

--- desafio14.sql
+++ desafio14.sql
@@ -1,7 +1,5 @@
-SELECT Country
-FROM w3schools.suppliers
+SELECT Country AS País FROM w3schools.suppliers
 UNION
-SELECT Country
-FROM w3schools.customers
-ORDER BY Country
+SELECT Country AS País FROM w3schools.customers
+ORDER BY País
 LIMIT 5;
