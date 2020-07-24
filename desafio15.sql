2b908f7a643e372c390a9ce47c98da71d71e1c81
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-07-24 17:24:26 -0300

    Update desafio15.sql

--- desafio15.sql
+++ desafio15.sql
@@ -4,11 +4,11 @@ delimiter $$
 drop procedure if exists buscar_media_por_cargo$$
 create procedure buscar_media_por_cargo(in cargo varchar(40))
 begin
-	select round(avg(salary), 2) as 'Média salarial'
-  from hr.employees as e
-  join hr.jobs as j
-  on j.JOB_ID = e.JOB_ID
-  where j.JOB_TITLE = cargo;
+select round(avg(salary), 2) as 'Média salarial'
+from hr.employees as e
+join hr.jobs as j
+on j.JOB_ID = e.JOB_ID
+where j.JOB_TITLE = cargo;
 end $$
 
 delimiter ;
