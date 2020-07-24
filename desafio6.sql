75955088dee9689a91e58849f11250705e28d059
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-23 23:27:30 -0300

    6.sql

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,12 @@
 SELECT
-CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS "Nome completo",
-(SELECT j.JOB_TITLE FROM hr.jobs AS j WHERE j.JOB_ID = e.JOB_ID LIMIT 1) AS Cargo,
-e.HIRE_DATE AS "Data de início do cargo",
+CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS `Nome completo`,
+(SELECT j.JOB_TITLE FROM hr.jobs AS j WHERE j.JOB_ID = emp.JOB_ID) AS Cargo,
+emp.HIRE_DATE AS "Data de início do cargo",
 (
   SELECT d.DEPARTMENT_NAME FROM hr.departments AS d
-  WHERE e.DEPARTMENT_ID = d.DEPARTMENT_ID LIMIT 1
+  WHERE emp.DEPARTMENT_ID = d.DEPARTMENT_ID
 ) AS "Departamento"
-FROM hr.employees AS e
-ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, Cargo ASC;
-SELECT * FROM hr.employees
-where CONCAT(FIRST_NAME, ' ', LAST_NAME) = 'Neena Kochhar';
-SELECT * FROM hr.jobs WHERE JOB_ID = "AD_VP";
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS emp
+ON jh.EMPLOYEE_ID = emp.EMPLOYEE_ID
+ORDER BY `Nome completo` DESC, CARGO;
