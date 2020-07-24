2c51edcbaf8f508fbdb735aba0226e4627c9a6d5
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-07-24 19:35:44 -0300

    Update desafio19.sql

--- desafio19.sql
+++ desafio19.sql
@@ -2,7 +2,7 @@ use hr;
 delimiter $$
 
 drop procedure if exists exibir_historico_completo_por_funcionario;
-create procedure exibir_historico_completo_por_funcionario(in email varchar(40))
+create procedure exibir_historico_completo_por_funcionario(in mail varchar(40))
 begin
 select concat(e.first_name, ' ', e.last_name) as 'Nome completo',
 d.department_name as 'Departamento',
@@ -11,7 +11,7 @@ from hr.employees as e
 join hr.job_history as jh on jh.employee_id = e.employee_id
 join hr.departments as d on jh.department_id = d.department_id
 join hr.jobs as j on jh.job_id = j.job_id
-where e.email = email
+where e.email = mail
 order by `Departamento`, `Cargo`;
 end $$
 
