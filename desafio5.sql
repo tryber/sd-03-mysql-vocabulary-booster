e743a54cd112a15e72e22f25e8b2e0582cf3b7a9
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-24 11:18:59 -0300

    5 order

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT
 j.JOB_TITLE AS Cargo,
-ROUND(j.MAX_SALARY - j.MIN_SALARY, 2) AS "Variação Salarial",
+ROUND(j.MAX_SALARY - j.MIN_SALARY, 2) AS `Variação Salarial`,
 ROUND(j.MIN_SALARY / 12, 2) AS "Média mínima mensal",
 ROUND(j.MAX_SALARY / 12, 2) AS "Média máxima mensal"
 FROM hr.jobs AS j
-ORDER BY ROUND(j.MAX_SALARY - j.MIN_SALARY, 2) ASC;
+ORDER BY `Variação Salarial`, Cargo;
