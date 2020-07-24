888573d1b8fced43a09b060f178d76fb3568a0b6
William Igor <williamigorsilva@gmail.com>
2020-07-24 18:21:31 -0300

    teste 5,6

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,6 @@
-SELECT * FROM hr.jobs;
-
 SELECT JOB_TITLE AS Cargo,
 MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
-AVG(MIN_SALARY) AS 'Média mínima mensal',
-AVG(MAX_SALARY) AS 'Média máxima mensal'
-from hr.jobs;
+ROUND(MIN_SALARY /12, 2) AS 'Média mínima mensal',
+ROUND(MAX_SALARY/12,2) AS 'Média máxima mensal'
+from hr.jobs
+ORDER BY `Variação Salarial` , Cargo;
--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,11 @@
 SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo', 
 c.JOB_TITLE as Cargo,
-HIRE_DATE AS 'Data de início do cargo',
-DEPARTMENT_ID AS Departamento
-FROM hr.employees
+h.START_DATE AS 'Data de início do cargo',
+ d.DEPARTMENT_NAME AS Departamento
+FROM hr.employees AS e
 INNER JOIN hr.jobs c
-ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME), Cargo DESC;
+INNER JOIN hr.departments d
+INNER JOIN hr.job_history h
+WHERE e.EMPLOYEE_ID = h.EMPLOYEE_ID AND c.JOB_ID = h.JOB_ID
+AND d.DEPARTMENT_ID = h.DEPARTMENT_ID
+ORDER BY `Nome completo`, Cargo DESC;
