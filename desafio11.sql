79af800a55a2f35ffcedd262073fa66ae52e7c14
Henrique Rezende <henriquerezendec@gmail.com>
2020-07-23 21:48:09 -0300

    Refactoring 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,19 +1,17 @@
 SELECT
-    C1.ContactName AS Nome,
-    C1.Country AS País,
-    (SELECT
-            COUNT(Country) - 1
-        FROM
-            w3schools.customers AS C2
-        WHERE
-            C2.Country = C1.Country) AS `Número de compatriotas`
+    *
 FROM
-    w3schools.customers AS C1
-WHERE
     (SELECT
-            COUNT(Country) - 1
-        FROM
-            w3schools.customers AS C2
-        WHERE
-            C2.Country = C1.Country) <> 0
-ORDER BY ContactName ASC;
+        C1.ContactName AS Nome,
+            C1.Country AS País,
+            (SELECT
+                    COUNT(Country) - 1
+                FROM
+                    w3schools.customers AS C2
+                WHERE
+                    C2.Country = C1.Country) AS `Número de compatriotas`
+    FROM
+        w3schools.customers AS C1) AS A
+WHERE
+    `Número de compatriotas` <> 0
+ORDER BY Nome ASC;
