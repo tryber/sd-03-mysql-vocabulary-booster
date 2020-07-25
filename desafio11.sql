817ec247e7e96e68ad85dcb3e33c2d5e13cba393
mirellaalves <mirella.alves@gmail.com>
2020-07-25 08:44:45 -0300

    desafio11

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,6 @@ cu1.Country `País`,
 COUNT(*) - 1 `Número de compatriotas`
 FROM w3schools.customers AS cu1, w3schools.customers AS cu2
 WHERE cu1.Country = cu2.Country
-GROUP BY `Nome`
+GROUP BY `Nome`, `País`
 HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;
