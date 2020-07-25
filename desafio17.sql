320d03734c7f0a1607c835edd950f67cd11972e0
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-07-25 17:14:11 -0300

    Exercicio18

--- desafio17.sql
+++ desafio17.sql
@@ -1,6 +1,3 @@
-SELECT * FROM hr.job_history;
-SELECT * FROM hr.employees;
-
 SELECT CONCAT(e.FIRST_NAME,' ',e.LAST_NAME) AS `Nome completo`,
 CONCAT(DAY(j.START_DATE),'/',MONTH(j.START_DATE),'/',YEAR(j.START_DATE)) AS 'Data de início',
 CONCAT(DAY(j.END_DATE),'/',MONTH(j.END_DATE),'/',YEAR(j.END_DATE)) AS 'Data de rescisão',
