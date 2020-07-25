8fe32513bceaed6e0b995aaa4c7583c49593f28d
Gabryel <gabryel.ryba@gmail.com>
2020-07-25 15:45:44 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,16 @@
+SELECT j.JOB_TITLE AS 'Cargo',
+AVG(e.SALARY) AS 'Diferença entre salários máximo e mínimo',
+CASE
+WHEN AVG(e.SALARY) BETWEEN 2000 AND 5800 
+THEN 'Júnior'
+WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 
+THEN 'Pleno'
+WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 
+THEN 'Sênior'
+ELSE 'CEO'
+END AS 'Senioridade'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON e.JOB_ID = j.JOB_ID
+GROUP BY j.JOB_TITLE
+ORDER BY ROUND(AVG(e.SALARY), 2) ASC;
