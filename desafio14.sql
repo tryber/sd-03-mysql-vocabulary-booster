7160ef8cf13c0c97f1ca6ffc6c0476959d36ba50
mirellaalves <mirella.alves@gmail.com>
2020-07-25 11:25:09 -0300

    desafio14

---
+++ desafio14.sql
@@ -0,0 +1,6 @@
+(SELECT Country `País` FROM w3schools.customers
+ORDER BY Country LIMIT 5)
+UNION
+(SELECT Country `País` FROM w3schools.suppliers
+ORDER BY Country LIMIT 5)
+ORDER BY `País` LIMIT 5;
