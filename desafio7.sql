230e62d50b75f51f3828322a3d285cda5c66b332
Gabryel <gabryel.ryba@gmail.com>
2020-07-25 16:39:59 -0300

    fixed desafio7.sql column name

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-jh.START_DATE AS 'Data de início do cargo',
+jh.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
