a7b2f9147a9c322d1c79a4a767e34a1f1e79d90a
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-07-25 16:18:41 -0300

    Exercicio10_16

--- desafio13.sql
+++ desafio13.sql
@@ -1,3 +1,4 @@
 SELECT ProductName AS `Produto`, Price AS `Preço`
 FROM w3schools.products
-WHERE ProductID in(SELECT ProductID FROM w3schools.order_details WHERE Quantity > 80);
+WHERE ProductID in(SELECT ProductID FROM w3schools.order_details WHERE Quantity > 80)
+ORDER BY `Produto`;
