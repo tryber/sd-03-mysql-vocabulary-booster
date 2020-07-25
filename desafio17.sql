789c5615612f81b0cf112fc4df9da277eb9872e4
Henrique Rezende <henriquerezendec@gmail.com>
2020-07-24 22:37:56 -0300

    17 fix

--- desafio17.sql
+++ desafio17.sql
@@ -5,8 +5,9 @@ SELECT
             hr.employees
         WHERE
             employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID) AS `Nome completo`,
-        START_DATE AS `Data de início`,
-        END_DATE AS `Data de rescisão`,
+        DATE_FORMAT(START_DATE, '%d/%m/%Y') AS `Data de início`,
+        DATE_FORMAT(END_DATE, '%d/%m/%Y') AS `Data de rescisão`,
         ROUND((END_DATE - START_DATE) / 8760, 2) AS `Anos trabalhados`
 FROM
-    hr.job_history;
+    hr.job_history
+WHERE YEAR(START_DATE) <> 0;
