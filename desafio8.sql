4e5aca58d7cd19e690f09abd935ff145384192f2
nato-x <alvares.renato@hotmail.com>
2020-07-24 21:14:26 -0300

    8

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,11 @@
 SELECT O.orderDate AS 'Data do pedido',
-customerName  AS 'Nome de contato',
+    C.customerName  AS 'Nome de contato',
     S.shipperName AS 'Empresa que fez o envio'
 FROM w3schools.customers AS C
     JOIN w3schools.orders AS O
     ON O.customerid = C.customerID
-    JOIN shippers AS S
+    JOIN w3schools.shippers AS S
     ON O.shipperID = S.shipperID AND 
     (S.shipperName = 'Speedy Express' OR S.shipperName = 'United Package')
 ORDER BY customerName, S.shipperName, O.orderDate;
-SELECT * FROM w3schools.orders;
+
