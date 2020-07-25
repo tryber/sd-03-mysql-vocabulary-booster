03cea5fc05c585df92606b7d1e75cce75df5cf97
Lucas Werner <lucas.1werner@gmail.com>
2020-07-25 20:36:02 -0300

    desafio 3

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+SELECT
+    CONCAT(E.FirstName, ' ', E.LastName) AS `Nome completo`,
+    COUNT(O.EmployeeID) AS `Total de pedidos`
+FROM
+    w3schools.orders AS O
+        INNER JOIN
+    w3schools.employees AS E
+WHERE
+    O.EmployeeID = E.EmployeeID
+GROUP BY O.EmployeeID
+ORDER BY `Total de pedidos`;
