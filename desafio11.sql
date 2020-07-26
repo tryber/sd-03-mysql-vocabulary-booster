6898b13a18cd4eb3a19aba8faf43163698c68816
luldelrey <rpgstrikered@gmail.com>
2020-07-26 12:04:24 -0300

    challenge 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,9 @@
-SELECT ContactName AS "Nome",
-Country AS "País",
-(
-  SELECT COUNT(CustomerID)
-  FROM w3schools.customers c
-  WHERE cust.Country = c.Country
+SELECT cust.ContactName AS "Nome",
+cust.Country AS "País",
+COUNT(c.CustomerID) AS "Número de compatriotas"
+FROM w3schools.customers cust,
+w3schools.customers c
+WHERE cust.Country = c.Country
   AND cust.CustomerID <> c.CustomerID
-) AS "Número de compatriotas"
-FROM w3schools.customers cust
-WHERE Country <> "Norway"
-AND Country <> "Ireland"
-AND Country <> "Poland"
+GROUP BY (cust.CustomerID)
 ORDER BY 1 ASC;
