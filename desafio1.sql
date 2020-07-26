8e901e95c8533488a90f696dafd7ea4631e5efe6
Lucas Franca <luc.rangel90@gmail.com>
2020-07-26 06:20:38 -0300

    Correção gramatical para passar no requisito

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
 SELECT country_name AS País,
-IF(region_id = 1, 'incluído', 'não incluido') AS Status_Inclusão
+IF(region_id = 1, 'incluído', 'não incluido') AS 'Status Inclusão'
 FROM hr.countries ORDER BY country_name ASC;
