a95536364eda9ef2c9232f48834001a811e5cf57
Lucas Franca <luc.rangel90@gmail.com>
2020-07-26 06:30:22 -0300

    Outras alteraçãoes

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
-SELECT country_name AS `País`,
-IF(region_id = 1, 'incluído', 'não incluido') AS 'Status Inclusão'
+SELECT COUNTRY_NAME AS `País`,
+IF(REGION_ID = 1, 'incluído', 'não incluido') AS 'Status Inclusão'
 FROM hr.countries
-ORDER BY `País` ASC;
+ORDER BY `País`;
