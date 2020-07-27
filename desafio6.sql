d75170d7b9329db943e2e15d750edbb2a576a467
rsfdev <roger_faustino@hotmail.com>
2020-07-27 19:08:04 -0300

    Challenge correction

--- desafio6.sql
+++ desafio6.sql
@@ -4,4 +4,4 @@ FS.START_DATE AS 'Data de início do cargo',
 DT.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees AS CN, hr.jobs AS FT, hr.departments AS DT, hr.job_history AS FS
 WHERE (FS.JOB_ID = FT.JOB_ID) AND (FS.DEPARTMENT_ID = DT.DEPARTMENT_ID) AND (FS.EMPLOYEE_ID = CN.EMPLOYEE_ID)
-ORDER BY 1, 2;
+ORDER BY `Nome completo` DESC, 2;
