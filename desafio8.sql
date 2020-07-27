4fe03fd783cb65cd9f42cf56bac066c9f7521dbe
Lucas Franca <luc.rangel90@gmail.com>
2020-07-27 13:45:39 -0300

    cc

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,7 @@
-SELECT customers.ContactName AS `Nome de contato`,
-  shippers.ShipperName AS `Empresa que fez o envio`,
-  orders.OrderDate AS `Data do pedido`
+SELECT
+customers.ContactName AS `Nome de contato`,
+shippers.ShipperName AS `Empresa que fez o envio`,
+orders.OrderDate AS `Data do pedido`
 FROM w3schools.orders
   INNER JOIN w3schools.shippers ON orders.ShipperID = shippers.ShipperID
   INNER JOIN w3schools.customers ON orders.CustomerID = customers.CustomerID
