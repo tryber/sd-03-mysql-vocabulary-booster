9de47afe071379a94d5f702ebe71bfe3e363287e
nato-x <alvares.renato@hotmail.com>
2020-07-27 12:06:50 -0300

    16,17

--- desafio16.sql
+++ desafio16.sql
@@ -1,12 +1,15 @@
-DELIMITER $$
+DELIMITER $
+$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario( inputEmail VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
-    DECLARE qt_empregados INT;
-    SELECT COUNT(job_id) FROM hr.employees
+    RETURN
+    SELECT COUNT(job_id)
+    FROM hr.employees
     WHERE job_id LIKE (
-        SELECT job_id FROM hr.employees WHERE email LIKE inputEmail)
-        INTO qt_empregados;
-	RETURN qt_empregados;
-END $$
+        SELECT job_id
+    FROM hr.employees
+    WHERE email LIKE inputEmail);
+END
+$$
 DELIMITER ;
--- desafio17.sql
+++ desafio17.sql
@@ -1,4 +1,4 @@
-SELECT 
+SELECT
     (SELECT CONCAT(first_name, ' ', last_name)
     FROM hr.employees
     WHERE employees.employee_id = job_history.employee_id) AS 'Nome completo',
@@ -8,7 +8,8 @@ SELECT
     CONCAT(DAY(end_date),'/', 
     MONTH(end_date), '/',
      YEAR(end_date)) AS 'Data de rescisão',
-ROUND((end_date - start_date) / 8760, 2)  AS 'Anos trabalhados'
-FROM hr.job_history 
+    ROUND((end_date - start_date) / 8760, 2)  AS 'Anos trabalhados'
+FROM hr.job_history
 WHERE employee_id <> 0
-ORDER BY `Nome completo`, `Anos trabalhados`;
+ORDER BY `Nome completo`, `Anos
+trabalhados`;
