6953cc9feed57e91eeebb8242d645a0801405e86
Cardoso <lsc.cardosoo@gmail.com>
2020-07-27 09:55:59 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,4 +3,5 @@ FROM w3schools.customers AS cust1
 INNER JOIN w3schools.customers AS cust2
 ON cust1.Country = cust2.Country
 GROUP BY cust1.ContactName, País
+HAVING `Número de compatriotas` <> 0
 ORDER BY Nome;
