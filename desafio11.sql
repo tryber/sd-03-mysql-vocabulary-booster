60efb4a7de5be337ec469a17ae87e0b25df7610c
Cardoso <lsc.cardosoo@gmail.com>
2020-07-27 09:42:01 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT cust1.ContactName AS Nome, cust1.Country AS País, COUNT(cust1.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS cust1
+INNER JOIN w3schools.customers AS cust2
+ON cust1.Country = cust2.Country
+GROUP BY cust1.ContactName, País
+ORDER BY Nome;
