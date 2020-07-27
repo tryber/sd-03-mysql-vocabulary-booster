e878ee1d14db97711a1d22574e095cc60fe56178
Henrique Rezende <henriquerezendec@gmail.com>
2020-07-27 15:14:53 -0300

    Desafio 19

--- desafio17.sql
+++ desafio17.sql
@@ -10,4 +10,5 @@ SELECT
         ROUND(DATEDIFF(END_DATE,START_DATE) / 365, 2) AS `Anos trabalhados`
 FROM
     hr.job_history
-WHERE YEAR(START_DATE) <> 0;
+WHERE YEAR(START_DATE) <> 0
+ORDER BY `Nome completo`, `Anos trabalhados`;
