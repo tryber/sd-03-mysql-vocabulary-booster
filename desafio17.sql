a83c28ef1a33c0542ed7b46a457958d10cb86f7d
clayton Pereira <cpereiramt@gmail.com>
2020-07-27 19:00:43 -0400

    Salvando alterações exercicio 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
 DATE_FORMAT(job_history.START_DATE, '%d/%m/%Y') AS "Data de início",
-DATE_FORMAT(job_history.END_DATE, '%d/%m/%Y') AS "Data de início",
+DATE_FORMAT(job_history.END_DATE, '%d/%m/%Y') AS "Data de rescisão",
   ROUND(DATEDIFF(job_history.END_DATE, job_history.START_DATE) / 365,
             2) AS `Anos trabalhados`
 FROM hr.employees
