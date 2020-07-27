a4c4a1c6fb8c4c0276652e2f0d3d54db25f4835e
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-07-27 11:05:45 -0300

    Correcting desafio12.sql

--- desafio12.sql
+++ desafio12.sql
@@ -5,5 +5,6 @@ CONCAT(eb.first_name, ' ', eb.last_name) AS `Nome completo funcionário 2`,
 eb.SALARY AS `Salário funcionário 2`,
 eb.phone_number AS `Telefone funcionário 2`
 FROM hr.employees AS ea, hr.employees AS eb
-WHERE CONCAT(ea.first_name, ' ', ea.last_name) <> CONCAT(eb.first_name, ' ', eb.last_name)
+WHERE CONCAT(ea.first_name, ' ', ea.last_name) <> CONCAT(eb.first_name, ' ', eb.last_name) AND
+ea.JOB_ID = eb.JOB_ID
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
