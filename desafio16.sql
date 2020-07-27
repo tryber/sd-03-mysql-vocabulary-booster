1c08581a2dbd285e510ab4fa3902668fcb67f194
clayton Pereira <cpereiramt@gmail.com>
2020-07-27 18:03:52 -0400

    Salvando alterações exercicio 16

---
+++ desafio16.sql
@@ -0,0 +1,22 @@
+USE hr;
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email varchar(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE id int;
+DECLARE quantidade int;
+SELECT 
+    employees.EMPLOYEE_ID
+FROM
+    hr.employees
+WHERE
+   employees.EMAIL = email INTO id;
+SELECT 
+    COUNT(*)
+FROM
+    hr.job_history
+WHERE
+    job_history.EMPLOYEE_ID = id INTO quantidade;
+RETURN quantidade;
+END $$
+DELIMITER ;
