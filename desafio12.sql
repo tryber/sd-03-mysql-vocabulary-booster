c43e7f10390bb07ba0289dcec72d6856d931e2ba
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-07-27 10:58:45 -0300

    Uploading desafio12.sql

---
+++ desafio12.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT(ea.first_name, ' ', ea.last_name) AS `Nome completo funcionário 1`,
+ea.SALARY AS `Salário funcionário 1`,
+ea.phone_number AS `Telefone funcionário 1`,
+CONCAT(eb.first_name, ' ', eb.last_name) AS `Nome completo funcionário 2`,
+eb.SALARY AS `Salário funcionário 2`,
+eb.phone_number AS `Telefone funcionário 2`
+FROM hr.employees AS ea, hr.employees AS eb
+WHERE CONCAT(ea.first_name, ' ', ea.last_name) <> CONCAT(eb.first_name, ' ', eb.last_name)
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
