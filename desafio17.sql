d03fd2f310ea043dc11087edffd35a578dc3c73e
clayton Pereira <cpereiramt@gmail.com>
2020-07-27 18:40:25 -0400

    Salvando alterações exercicio 17

---
+++ desafio17.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
+DATE_FORMAT(job_history.START_DATE, '%d/%m/%Y') AS "Data de início",
+DATE_FORMAT(job_history.END_DATE, '%d/%m/%Y') AS "Data de início",
+ ROUND(DATEDIFF(job_history.END_DATE, job_history.START_DATE) / 365,
+            2) AS `Anos trabalhados`
+FROM hr.employees
+INNER JOIN hr.job_history 
+ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+ORDER BY 1;
