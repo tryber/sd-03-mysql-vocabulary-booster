6049d26a80d2178b2d5dfa6e33ff9c4918d82184
mirellaalves <mirella.alves@gmail.com>
2020-07-27 18:57:17 -0300

    correct desafio15

--- desafio15.sql
+++ desafio15.sql
@@ -1,18 +1,17 @@
 USE hr;
 DELIMITER $$
 
-CREATE FUNCTION buscar_media_por_cargo(cargo VARCHAR(100))
-RETURNS INT READS SQL DATA
+CREATE PROCEDURE buscar_media_por_cargo(
+IN cargo VARCHAR(100)
+)
 BEGIN
-DECLARE media_salario INT;
-SELECT ROUND(AVG(SALARY), 2)
+SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
 FROM hr.employees em
 INNER JOIN hr.jobs jb
 ON em.JOB_ID = jb.JOB_ID
-WHERE jb.JOB_TITLE = cargo INTO media_salario;
-RETURN media_salario;
+WHERE jb.JOB_TITLE = cargo;
 END $$
 
 DELIMITER ;
 
-SELECT buscar_media_por_cargo('Programmer');
+CALL buscar_media_por_cargo('Programmer');
