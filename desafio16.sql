de607f1d19a8e6e3c1d0cbf5103fdd665ff8323c
mirellaalves <mirella.alves@gmail.com>
2020-07-27 20:32:29 -0300

    desafio16

--- desafio16.sql
+++ desafio16.sql
@@ -1,1 +1,16 @@
 USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(25))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE job_amount INT;
+SELECT COUNT(*) FROM hr.job_history jh
+INNER JOIN hr.employees em ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
+WHERE em.EMAIL = email INTO job_amount;
+RETURN job_amount;
+END $$
+
+DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
