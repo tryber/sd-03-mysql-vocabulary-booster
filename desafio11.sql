db9f60dcd555c0cc667a1c3be76fc0ad5c6bbcd6
Lucas Franca <luc.rangel90@gmail.com>
2020-07-28 03:07:38 -0300

    desafio 11 feito

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT * FROM customers;
+
+SELECT customers.ContactName AS `Nome`,
+customers.Country AS `País`,
+COUNT(customers_B.Country) AS 'Número de compatriotas'
+FROM w3schools.customers AS customers,
+w3schools.customers AS customers_B
+WHERE customers.Country = customers_B.Country AND customers.CustomerID <> customers_B.CustomerID
+GROUP BY `Nome`, `País`
+ORDER BY `Nome`;
