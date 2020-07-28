0ec02a70943819cb8fae5d041df287313ecfc297
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-07-28 19:18:49 -0300

    Desafio 13 finalizado

--- desafio13.sql
+++ desafio13.sql
@@ -4,4 +4,5 @@ p.Price AS 'Preço',
 FROM w3schools.order_details AS od
 JOIN w3schools.products AS p
 ON od.ProductID = p.ProductID
-HAVING od.Quantity > 80;
+HAVING od.Quantity > 80
+ORDER BY p.ProductName;
