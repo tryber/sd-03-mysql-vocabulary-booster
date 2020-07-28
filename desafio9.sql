3767d379dda1956a258d4398509e6d35f70f16a8
Lucas Franca <luc.rangel90@gmail.com>
2020-07-28 03:58:05 -0300

    desafios 13, 14 e 15

--- desafio9.sql
+++ desafio9.sql
@@ -1,7 +1,7 @@
-SELECT CONCAT(employees.FirstName, " ", employees.Lastname) AS 'Nome Completo',
+SELECT CONCAT(employees.FirstName, ' ', employees.Lastname) AS `Nome completo`,
 COUNT(orders.EmployeeID) AS `Total de pedidos`
 FROM w3schools.employees AS employees
 INNER JOIN w3schools.orders AS orders
 ON employees.EmployeeID = orders.EmployeeID
-GROUP BY (orders.EmployeeID)
+GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`;
