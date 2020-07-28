f80423f54c2d2865ff8573f31af03b3215c579fa
Lucas Franca <luc.rangel90@gmail.com>
2020-07-28 04:14:21 -0300

    desafio 13 e 18

--- desafio16.sql
+++ desafio16.sql
@@ -3,11 +3,11 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(300)
 RETURNS TINYINT READS SQL DATA
 BEGIN
   DECLARE jobs_num TINYINT;
-  SELECT COUNT(jh.EMPLOYEE_ID) AS total_empregos
-  FROM hr.employees AS emp
-  INNER JOIN hr.job_history AS jh
-  ON jh.EMPLOYEE_ID = emp.EMPLOYEE_ID
-  WHERE emp.EMAIL = email
+  SELECT COUNT(history.EMPLOYEE_ID) AS total_empregos
+  FROM hr.employees AS employees
+  INNER JOIN hr.job_history AS history
+  ON history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+  WHERE employees.EMAIL = email
   INTO jobs_num;
   RETURN jobs_num;
 END $$
