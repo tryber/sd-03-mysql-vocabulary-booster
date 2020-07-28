e8924b3ddefa93ec431a8a19edee6661adac6cd4
William Igor <williamigorsilva@gmail.com>
2020-07-27 21:20:14 -0300

    concertos

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,15 @@
-SELECT  CONCAT(P.FirstName, ' ', P.LastName) as `Nome completo`,
-COUNT(P.EmployeeID) AS  `Total de pedidos`
-FROM w3schools.employees as P
-INNER JOIN w3schools.orders as O
-WHERE O.EmployeeID = P.EmployeeID
-GROUP BY O.EmployeeID
-ORDER BY `Total de pedidos`;
+SELECT 
+a.ContactName AS `Nome de contato`,
+b.ShipperName AS `Empresa que fez o envio`,
+c.OrderDate AS `Data do pedido`
+FROM
+w3schools.customers AS a
+INNER JOIN
+w3schools.shippers AS b
+INNER JOIN
+w3schools.orders AS c
+WHERE
+c.CustomerID = a.CustomerID
+AND b.ShipperID = c.ShipperID
+AND b.ShipperID IN (1 , 2)
+ORDER BY `Nome de contato` ASC , `Empresa que fez o envio` ASC , `Data do pedido` ASC;
