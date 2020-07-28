f553a8c3f73b852672805467bffa97c462cd3fb4
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-07-28 15:43:48 -0300

    Desafio 10 finalizado

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT
+p.ProductName AS 'Produto',
+MIN(od.Quantity) AS 'Mínima',
+MAX(od.Quantity) AS 'Máxima',
+ROUND(AVG(od.Quantity), 2) AS 'Média'
+FROM w3schools.order_details AS od
+JOIN w3schools.products AS p
+ON od.ProductID = p.ProductID
+WHERE od.Quantity > 20.00
+GROUP BY od.ProductID
+ORDER BY AVG(od.Quantity), p.ProductName;
