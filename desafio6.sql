9975b11df022dc05735007e75724e2f2b2643591
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-07-28 09:03:00 -0300

    upando

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ SELECT
 CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
 j.JOB_TITLE AS Cargo,
 jobh.START_DATE
- AS 'Data de início do cargo',
+AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS Departamento
 FROM hr.employees e
 INNER JOIN hr.jobs j ON e.JOB_ID = j.JOB_ID
