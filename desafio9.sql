4ee7084cdd937dab94e4d0cae73c070dd7ab7a90
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-07-28 10:31:30 -0300

    upando

---
+++ desafio9.sql
@@ -0,0 +1,3 @@
+SELECT CONCAT(FirstName, ' ',LastName) 'Nome completo', COUNT(ord.OrderID) 'Total de pedidos' FROM w3schools.employees emp
+INNER JOIN w3schools.orders ord ON ord.EmployeeID = emp.EmployeeID
+GROUP BY ord.EmployeeID;
