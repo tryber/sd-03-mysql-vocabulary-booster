c03c5469445e0a007757fad64808ee867dcd1d28
Lucas Franca <luc.rangel90@gmail.com>
2020-07-28 03:50:28 -0300

    desafio 12 feito, 5 corrigido

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,7 @@ customers.Country AS `País`,
 COUNT(customers_B.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS customers,
 w3schools.customers AS customers_B
-WHERE customers.Country = customers_B.Country AND customers.CustomerID <> customers_B.CustomerID
+WHERE customers.Country = customers_B.Country
+AND customers.CustomerID <> customers_B.CustomerID
 GROUP BY `Nome`, `País`
 ORDER BY `Nome`;
--- desafio7.sql
+++ desafio7.sql
@@ -5,7 +5,7 @@ employees.SALARY AS 'Salário'
 FROM hr.job_history AS history
 JOIN hr.employees AS employees
 ON history.EMPLOYEE_ID = employees.EMPLOYEE_ID
-WHERE START_DATE LIKE '%01'
+WHERE START_DATE LIKE '%01%'
 OR START_DATE LIKE '%02%'
 OR START_DATE LIKE '%03%'
 ORDER BY `Nome completo`, `Data de início`;
