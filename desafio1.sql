097788aeb44d654f06b5b88e5c79e396f4cc8d8e
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-07-28 11:14:46 -0300

    Desafio 1 finalizado

---
+++ desafio1.sql
@@ -0,0 +1,4 @@
+SELECT c.COUNTRY_NAME AS 'País', IF(r.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries as c
+JOIN hr.regions as r
+ON c.REGION_ID = r.REGION_ID;
