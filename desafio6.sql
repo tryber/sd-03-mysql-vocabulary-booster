b235d09d90afe79cc771b78557e042d56960b2de
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-07-29 05:21:40 -0300

    ID link corrigido

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,8 @@ SELECT concat(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo',
 j.JOB_TITLE AS Cargo,
 jh.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS Departamento
-FROM hr.employees e INNER JOIN hr.jobs j ON e.JOB_ID = j.JOB_ID
-INNER JOIN hr.departments d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+FROM hr.employees e 
 INNER JOIN hr.job_history jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.jobs j ON j.JOB_ID = jh.JOB_ID
+INNER JOIN hr.departments d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 ORDER BY concat(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, Cargo;
