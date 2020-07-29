94f1194f37a3ee0fc02aa8af75bc9e9e1302a04d
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-07-29 00:33:18 -0300

    Desafio 15 refatorado

--- desafio15.sql
+++ desafio15.sql
@@ -1,12 +1,12 @@
 USE hr;
 DELIMITER $$
-CREATE PROCEDURE buscar_media_por_cargo(IN job_name VARCHAR(100), OUT media_wage DOUBLE)
+CREATE PROCEDURE buscar_media_por_cargo(IN job_name VARCHAR(100))
 BEGIN
 SELECT
-ROUND(AVG(e.SALARY), 2) AS 'Média salarial' INTO media_wage
+ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
 FROM hr.employees AS e
 JOIN hr.jobs AS j
 ON e.JOB_ID = j.JOB_ID
-WHERE j.JOB_TITLE = job_name;
+WHERE j.JOB_TITLE = job_name
 END $$
 DELIMITER ;
