190e2fd8abeb8b6aa21c60565a092c794edadd0a
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-07-29 00:29:42 -0300

    Desafio 14 refatorado

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-SELECT Country AS 'País'  FROM w3schools.customers
+SELECT Country AS 'País'  FROM w3schools.suppliers
 UNION
-SELECT Country AS 'País' FROM w3schools.suppliers
+SELECT Country AS 'País' FROM w3schools.customers
 ORDER BY `País`
 LIMIT 5;
