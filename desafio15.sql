c970e24273b6f96190f7b29fae98a23a1c684f48
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-07-28 23:25:12 -0300

    Desafio 16 finalizado

--- desafio15.sql
+++ desafio15.sql
@@ -7,8 +7,7 @@ ROUND(AVG(e.SALARY), 2) AS 'Média salarial' INTO media_wage
 FROM hr.employees AS e
 JOIN hr.jobs AS j
 ON e.JOB_ID = j.JOB_ID
-WHERE j.JOB_TITLE= job_name
-GROUP BY e.JOB_ID;
+WHERE j.JOB_TITLE= job_name;
 END $$
 DELIMITER ;
 CALL buscar_media_por_cargo('Programmer', @media_wage_return);
