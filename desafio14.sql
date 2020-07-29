7df6f982f71be1dc6e8330202f37493410b087fe
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-07-29 13:50:49 -0300

    Países

---
+++ desafio14.sql
@@ -0,0 +1,6 @@
+SELECT Country AS País 
+FROM ((SELECT Country FROM w3schools.customers GROUP BY Country)
+UNION
+(SELECT Country FROM w3schools.suppliers GROUP BY Country)) Country
+ORDER BY País
+LIMIT 5;
