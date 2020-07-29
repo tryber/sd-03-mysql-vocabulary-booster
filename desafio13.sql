037046b747d36f16892c697463f358ae5450bde0
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-07-29 00:26:41 -0300

    Desafio 13 refatorado

--- desafio13.sql
+++ desafio13.sql
@@ -1,8 +1,11 @@
-SELECT
-p.ProductName AS 'Produto',
-p.Price AS 'Preço',
-FROM w3schools.order_details AS od
-JOIN w3schools.products AS p
-ON od.ProductID = p.ProductID
-WHERE od.Quantity > 80
+SELECT 
+ProductName AS `Produto`,
+Price AS 'Preço'
+FROM
+w3schools.products
+WHERE ProductID IN (SELECT 
+ProductID
+FROM
+w3schools.order_details
+WHERE Quantity > 80)
 ORDER BY `Produto`;
