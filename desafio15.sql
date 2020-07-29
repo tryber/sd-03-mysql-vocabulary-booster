71effd48970f9abd6ebadab0eb8c503042bd0cf7
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-07-28 23:03:58 -0300

    Desafio 15 finalizado

---
+++ desafio15.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE buscar_media_por_cargo(IN job_name VARCHAR(100), OUT media_wage DOUBLE)
+BEGIN
+SELECT
+ROUND(AVG(e.SALARY), 2) AS 'Média salarial' INTO media_wage
+FROM hr.employees AS e
+JOIN hr.jobs AS j
+ON e.JOB_ID = j.JOB_ID
+WHERE j.JOB_TITLE= job_name
+GROUP BY e.JOB_ID;
+END $$
+DELIMITER ;
+CALL buscar_media_por_cargo('Programmer', @media_wage_return);
+SELECT @media_wage_return;
