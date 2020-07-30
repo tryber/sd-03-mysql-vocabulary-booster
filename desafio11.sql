4d295d31631eaf15fb9f982c9941e0f06f1d5725
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-07-30 16:16:37 -0300

    desafio11

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT a.ContactName AS `Nome`, a.Country AS `País`,
+COUNT(b.ContactName) AS `Número de compatriotas`
+FROM
+w3schools.customers AS a,
+w3schools.customers AS b
+WHERE
+a.ContactName <> b.ContactName AND a.Country = b.Country
+GROUP BY a.CustomerID
+ORDER BY `Nome`;
