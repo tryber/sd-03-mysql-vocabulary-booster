a4198974d0d97c5c087ff53d1b0679ae135c4cea
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-07-30 00:47:59 -0300

    Desafio9 issue

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(FirstName, ' ', LastName) AS `Nome completo`,
+SELECT CONCAT(employees.FirstName, ' ', employees.LastName) AS `Nome completo`,
 COUNT(orders.EmployeeID) AS `Total de pedidos`
 FROM w3schools.employees AS employees
 INNER JOIN w3schools.orders AS orders
