f17092362211fc68450ac05fd2ce04be689d32c5
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-07-30 00:23:18 -0300

    desafio8 issue

--- desafio8.sql
+++ desafio8.sql
@@ -3,8 +3,8 @@ shippers.ShipperName AS `Empresa que faz o envio`,
 orders.OrderDate AS `Data do pedido`
 FROM w3schools.orders AS orders
 INNER JOIN w3schools.customers AS customers
-ON customers.CustomerID = orders.CustomerID
+ON orders.CustomerID = customers.CustomerID
 INNER JOIN w3schools.shippers AS shippers
 ON orders.ShipperID = shippers.ShipperID
-WHERE shippers.ShipperName = 'Speedy Express' OR shippers.ShipperName = 'United Package'
+WHERE shippers.ShipperName IN ('Speedy Express', 'United Package')
 ORDER BY `Nome do contato`, `Empresa que faz o envio`, `Data do pedido`;
