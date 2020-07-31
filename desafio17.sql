709a07a1c37f52d10ff55a733c8b386dd76f1046
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-07-31 10:19:13 -0300

    desafio18

--- desafio17.sql
+++ desafio17.sql
@@ -6,8 +6,6 @@ DATE_FORMAT(h.START_DATE, '%d/%m/%Y') AS `Data de início`,
 DATE_FORMAT(h.END_DATE, '%d/%m/%Y') AS `Data de rescisão`,
 ROUND(DATEDIFF(h.END_DATE, h.START_DATE) / 365,
 2) AS `Anos trabalhados`
-FROM
-hr.employees AS e
-INNER JOIN
-hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 ORDER BY `Nome completo` , `Anos trabalhados`;
