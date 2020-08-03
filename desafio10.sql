f0115eefd8846cf21effb99c24ee51273ef48963
Hfreitas <hfreitas86@gmail.com>
2020-08-03 17:49:34 -0300

    commitando a pedido do Cacique para correção de database da Trybe

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,9 @@
 SELECT products.ProductName AS 'Produto',
-  MIN(order_details.Quantity) AS 'Mínima',
-  MAX(order_details.Quantity) AS 'Máxima',
-  ROUND(AVG(order_details.Quantity), 2) AS `Média`
+  MIN(od.Quantity) AS 'Mínima',
+  MAX(od.Quantity) AS 'Máxima',
+  ROUND(AVG(od.Quantity), 2) AS `Média`
 FROM w3schools.products AS products
-  INNER JOIN w3schools.order_details AS order_details ON order_details.ProductID = products.ProductID
+  INNER JOIN w3schools.order_details AS od ON od.ProductID = products.ProductID
 GROUP BY products.ProductName
 HAVING `Média` > 20.00
 ORDER BY `Média` ASC,
