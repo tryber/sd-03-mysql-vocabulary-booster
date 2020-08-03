d656be4138ca385b5004f0b3987422b917423441
tamagoshii <estelaap_martins@hotmail.com>
2020-08-03 12:16:12 -0300

    desafio do 1 até 5

---
+++ desafio4.sql
@@ -0,0 +1,16 @@
+SELECT job_title AS 'Cargo',
+ROUND(AVG(employee.salary), 2) AS 'Média salarial',
+CASE 
+    WHEN ROUND(AVG(employee.salary), 2) BETWEEN 2000 AND 5800  
+		THEN 'Júnior'
+    WHEN ROUND(AVG(employee.salary), 2) BETWEEN 5800 AND 7500
+		THEN 'Pleno'
+    WHEN ROUND(AVG(employee.salary), 2) BETWEEN 7500 AND 10500
+		THEN 'Sênior'
+	ELSE 'CEO'
+END AS 'Senioridade'
+FROM hr.jobs job
+    INNER JOIN hr.employees AS employee
+    ON job.job_id = employee.job_id
+GROUP BY job_title
+ORDER BY `Média salarial`, job_title;
