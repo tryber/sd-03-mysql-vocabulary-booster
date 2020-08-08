82ed38f085d8f878460a26dbe0afa635ef377e9b
Mateus Moreira Sarmento <60623845+Mateus-Moreira@users.noreply.github.com>
2020-08-08 00:40:25 -0300

    Update desafio8.sql

--- desafio8.sql
+++ desafio8.sql
@@ -1,13 +1,10 @@
-SELECT 
-    c.ContactName AS 'Nome de contato',
-    s.ShipperName AS 'Empresa que fez o envio',
-    o.OrderDate AS 'Data do pedido'
-FROM
-    w3schools.orders AS o
-        JOIN
-    w3schools.shippers AS s ON s.ShipperID = s.ShipperID
-        JOIN
-    w3schools.customers AS c ON c.CustomerID = a.CustomerID
-WHERE
-    s.ShipperID <> 3
-ORDER BY c.ContactName , s.ShipperName , o.OrderDate;
+SELECT C.ContactName AS "Nome de contato",
+B.ShipperName AS "Empresa que fez o envio",
+A.OrderDate AS "Data do pedido"
+FROM w3schools.orders AS A
+JOIN w3schools.shippers AS B
+ON B.ShipperID = A.ShipperID
+JOIN w3schools.customers AS C
+ON C.CustomerID = A.CustomerID
+WHERE B.ShipperID <> 3
+ORDER BY C.ContactName, B.ShipperName, A.OrderDate;
